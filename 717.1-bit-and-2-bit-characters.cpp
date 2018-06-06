72ddfb07d52b469efe4532ea3d9bc536314a69ca
sunznx <sunznx@163.com>
2018-06-06 14:56:23 +0800

    update 717.1-bit-and-2-bit-characters.cpp

--- 717.1-bit-and-2-bit-characters.cpp
+++ 717.1-bit-and-2-bit-characters.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     bool isOneBitCharacter(vector<int>& bits) {
-        return isOneBitCharacter(bits, 0, false, false);
-    }
-
-    bool isOneBitCharacter(vector<int>& bits, int from, bool found0, bool found1) {
         int len = bits.size();
-        for (int i = from; i < len; i++) {
+        for (int i = 0; i < len; i++) {
             if (bits[i] == 1) {
                 if (bits[i+1] == 0 && i+1 == len-1) {
                     return false;
