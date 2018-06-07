481ff8a8ed420efee9ac3405c6288072c7c56608
sunznx <sunznx@163.com>
2018-06-07 18:05:51 +0800

    update 836.rectangle-overlap.py

--- 836.rectangle-overlap.py
+++ 836.rectangle-overlap.py
@@ -6,5 +6,5 @@ class Solution:
         :rtype: bool
         """
         x11, y11, x21, y21 = rec1
-        rec2_x1, rec2_y1, rec2_x2, rec2_y2 = rec2
-        return not (x11 >= rec2_x2 or x21 <= rec2_x1 or y11 >= rec2_y2 or y21 <= rec2_y1)
+        x12, y12, x22, y22 = rec2
+        return not (x11 >= x22 or x21 <= x12 or y11 >= y22 or y21 <= y12)
