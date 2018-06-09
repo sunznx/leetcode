55f8bc3beb680a311c3ac5dc26cd26a2a707a959
sunznx <sunznx@163.com>
2018-06-09 19:41:28 +0800

    update 307.range-sum-query-mutable.cpp

--- 307.range-sum-query-mutable.cpp
+++ 307.range-sum-query-mutable.cpp
@@ -3,31 +3,35 @@ public:
     vector<int> data;
     vector<int> tree;
     vector<int> lazy;
+    int n;
 
     NumArray(vector<int> nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            data.push_back(nums[i]);
+        n = nums.size();
+        data = nums;
+        for (int i = 0; i < n; i++) {
+            data.push_back(0);
         }
-        for (int i = 0; i <= 3*data.size(); i++) {
+        for (int i = 0; i < 3*n; i++) {
             tree.push_back(0);
         }
-        for (int i = 0; i <= 3*data.size(); i++) {
+        for (int i = 0; i < 3*n; i++) {
             lazy.push_back(0);
         }
 
-        build(0, 0, nums.size()-1);
+        build(0, 0, n-1);
     }
 
     int left(int x) {
-        return 2*x + 1;
+        return x * 2 + 1;
     }
 
     int right(int x) {
-        return 2*x + 2;
+        return x * 2 + 2;
     }
 
     int mid(int l, int r) {
-        return (l+r)/2;
+        int m = (l + r) / 2;
+        return m;
     }
 
     void build(int root, int l, int r) {
@@ -40,41 +44,41 @@ public:
             return;
         }
 
-        build(left(root), l, mid(l,r));
-        build(right(root), mid(l,r)+1, r);
+        int m = mid(l, r);
+        build(left(root), l,    m);
+        build(right(root), m+1, r);
+
         tree[root] = tree[left(root)] + tree[right(root)];
     }
 
-    void update(int i, int val) {
-        int diff = val - data[i];
-        data[i] = val;
-        updateInterval(i, diff, 0, 0, data.size()-1);
+    void update(int index, int val) {
+        int diff = val - data[index];
+        data[index] = val;
+        updateInterval(index, index, diff, 0, 0, n-1);
     }
 
-    void updateInterval(int index, int diff, int root, int l, int r) {
+    void updateInterval(int i, int j, int diff, int root, int l, int r) {
         if (lazy[root] != 0) {
-            tree[root] += lazy[root];
-
+            tree[root] += (r-l+1) * lazy[root];
             if (l != r) {
                 lazy[left(root)]  += lazy[root];
                 lazy[right(root)] += lazy[root];
             }
-
             lazy[root] = 0;
         }
 
         /* 完全不包含 */
-        if (l > r || l > index || r < index) {
+        if (l > r || l > j || r < i) {
             return;
         }
 
         /* 全部包含 */
-        if (index <= l && r <= index) {
-            tree[root] += diff;
+        if (i <= l && r <= j) {
+            tree[root] += (r-l+1) * diff;
 
             if (l != r) {
-                lazy[left(root)]  = diff;
-                lazy[right(root)] = diff;
+                lazy[left(root)]  += diff;
+                lazy[right(root)] += diff;
             }
 
             return;
@@ -82,20 +86,17 @@ public:
 
         /* 一部分包含 */
         int m = mid(l, r);
-
-        updateInterval(index, diff, left(root),  l,   m);
-        updateInterval(index, diff, right(root), m+1, r);
-
+        updateInterval(i, j, diff, left(root),  l,   m);
+        updateInterval(i, j, diff, right(root), m+1, r);
         tree[root] = tree[left(root)] + tree[right(root)];
     }
 
-    int sumRange(int i, int j) {
-        return sumRange(i, j, 0, 0, data.size()-1);
+    int sumRange(int query_min, int query_max) {
+        return sumRange(query_min, query_max, 0, 0, n-1);
     }
-
     int sumRange(int query_min, int query_max, int root, int l, int r) {
         if (lazy[root] != 0) {
-            tree[root] += lazy[root];
+            tree[root] += (r-l+1) * lazy[root];
 
             if (l != r) {
                 lazy[left(root)]  += lazy[root];
