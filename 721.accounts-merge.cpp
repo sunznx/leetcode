b9306ee16e9fc8b53589e1a3e2bd59306d1bafae
sunznx <sunznx@163.com>
2018-06-09 17:08:43 +0800

    update 721.accounts-merge.cpp

--- 721.accounts-merge.cpp
+++ 721.accounts-merge.cpp
@@ -1,34 +1,49 @@
+// CreateTime: 2018-06-09 16:41:28
 class Solution {
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& acts) {
-        map<string, string> owner;
-        map<string, string> parents;
+    string uf_find(map<string, string> &parent, string email) {
+        while (email != parent[email]) {
+            email = parent[email];
+        }
+        return email;
+    }
+
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        vector<vector<string>> res;
+        map<string, string> parent;
+        map<string, string> username_map;
         map<string, set<string>> unions;
-        for (int i = 0; i < acts.size(); i++) {
-            for (int j = 1; j < acts[i].size(); j++) {
-                parents[acts[i][j]] = acts[i][j];
-                owner[acts[i][j]] = acts[i][0];
+
+        // init
+        for (int i = 0; i < accounts.size(); i++) {
+            string username = accounts[i][0];
+            for (int j = 1; j < accounts[i].size(); j++) {
+                parent[accounts[i][j]] = accounts[i][j];
+                username_map[accounts[i][j]] = username;
             }
         }
-        for (int i = 0; i < acts.size(); i++) {
-            string p = find(acts[i][1], parents);
-            for (int j = 2; j < acts[i].size(); j++)
-                parents[find(acts[i][j], parents)] = p;
-        }
-        for (int i = 0; i < acts.size(); i++)
-            for (int j = 1; j < acts[i].size(); j++)
-                unions[find(acts[i][j], parents)].insert(acts[i][j]);
 
-        vector<vector<string>> res;
+        for (int i = 0; i < accounts.size(); i++) {
+            string p1 = uf_find(parent, accounts[i][1]);
+            for (int j = 2; j < accounts[i].size(); j++) {
+                string p2 = uf_find(parent, accounts[i][j]);
+                parent[p2] = p1;
+            }
+        }
+
+        for (int i = 0; i < accounts.size(); i++) {
+            for (int j = 1; j < accounts[i].size(); j++) {
+                string p = uf_find(parent, accounts[i][j]);
+                unions[p].insert(accounts[i][j]);
+            }
+        }
+
         for (pair<string, set<string>> p : unions) {
             vector<string> emails(p.second.begin(), p.second.end());
-            emails.insert(emails.begin(), owner[p.first]);
+            emails.insert(emails.begin(), username_map[p.first]);
             res.push_back(emails);
         }
+
         return res;
-    }
-private:
-    string find(string s, map<string, string>& p) {
-        return p[s] == s ? s : find(p[s], p);
     }
 };
