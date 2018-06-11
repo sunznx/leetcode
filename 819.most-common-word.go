d8b06c736123550c0548c9ce9e1994103a1bf92d
sunznx <sunznx@163.com>
2018-06-11 22:15:28 +0800

    update 819.most-common-word.go

---
+++ 819.most-common-word.go
@@ -0,0 +1,36 @@
+func normalize(str string) string {
+	return strings.ToLower(strings.TrimRight(str, "!?',;."))
+}
+
+func mostCommonWord(paragraph string, banned []string) string {
+	m := make(map[string]int)
+	ban := make(map[string]bool)
+
+	for i := 0; i < len(banned); i++ {
+		str := normalize(banned[i])
+		ban[strings.ToLower(str)] = true
+	}
+
+	arr := strings.Split(paragraph, " ")
+
+	res := ""
+	maxn := 0
+
+	for i := 0; i < len(arr); i++ {
+		str := normalize(arr[i])
+		_, ok := ban[str]
+		if ok {
+			continue
+		}
+
+		m[str] += 1
+		// fmt.Printf("%v = %v\n", str, m[str])
+
+		if m[str] > maxn {
+			maxn = m[str]
+			res = str
+		}
+	}
+
+	return res
+}
