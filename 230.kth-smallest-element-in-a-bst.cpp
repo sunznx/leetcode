5a5c5c1f7635fb3656219cd4ed01e83807f0083e
sunznx <sunznx@163.com>
2018-06-13 01:21:42 +0800

    update 230.kth-smallest-element-in-a-bst.cpp

---
+++ 230.kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int cnt = 0;
+    int res = 0;
+
+    int kthSmallest(TreeNode* root, int k) {
+        dfs(root, k);
+        return res;
+    }
+    
+    void dfs(TreeNode *root, int k) {
+        if (cnt > k) {
+            return;
+        }
+        if (root->left) {
+            dfs(root->left, k);
+        }
+        cnt += 1;
+        if (cnt == k) {
+            res = root->val;
+            return;
+        }
+        if (root->right) {
+            dfs(root->right, k);
+        }
+    }
+};
