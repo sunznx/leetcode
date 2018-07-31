09e1a76ce32c3ffc2002dba5a1c6ef0f3595e067
sunznx <sunznx@163.com>
2018-07-30 20:46:15 -0500

    update 22.generate-parentheses.cpp

---
+++ 22.generate-parentheses.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<string> res;
+
+    vector<string> generateParenthesis(int n) {
+        if (n == 0) {
+            return res;
+        }
+
+        string v = "";
+        dfs(n, v, 0, 0);
+        return res;
+    }
+
+    void dfs(int n, string &v, int l, int r) {
+        if (l > n || r > n) {
+            return;
+        }
+
+        if (2 * n == l+r) {
+            res.push_back(v);
+            return;
+        }
+
+        if (l == n) {
+            v = v + ")";
+            dfs(n, v, l, r + 1);
+        } else if (r == n) {
+            return;
+        } else if (l == r) {
+            v = v + "(";
+            dfs(n, v, l+1, r);
+        } else {
+            auto tmp = v;
+
+            v = tmp;
+            v = v + "(";
+            dfs(n, v, l+1, r);
+
+            v = tmp;
+            v = v + ")";
+            dfs(n, v, l, r+1);
+        }
+    }
+};
