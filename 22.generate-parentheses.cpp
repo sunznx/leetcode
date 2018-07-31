fb85943c2330e920e2a07b4d58df4046cea115cc
sunznx <sunznx@163.com>
2018-07-30 20:48:29 -0500

    update 22.generate-parentheses.cpp

--- 22.generate-parentheses.cpp
+++ 22.generate-parentheses.cpp
@@ -13,7 +13,7 @@ public:
     }
 
     void dfs(int n, string &v, int l, int r) {
-        if (l > n || r > n) {
+        if (l > n || r > n || r > l) {
             return;
         }
 
@@ -22,24 +22,34 @@ public:
             return;
         }
 
-        if (l == n) {
-            v = v + ")";
-            dfs(n, v, l, r + 1);
-        } else if (r == n) {
-            return;
-        } else if (l == r) {
-            v = v + "(";
-            dfs(n, v, l+1, r);
-        } else {
-            auto tmp = v;
+        auto tmp = v;
 
-            v = tmp;
-            v = v + "(";
-            dfs(n, v, l+1, r);
+        v = tmp;
+        v = v + "(";
+        dfs(n, v, l+1, r);
 
-            v = tmp;
-            v = v + ")";
-            dfs(n, v, l, r+1);
-        }
+        v = tmp;
+        v = v + ")";
+        dfs(n, v, l, r+1);
+
+        // if (l == n) {
+        //     v = v + ")";
+        //     dfs(n, v, l, r + 1);
+        // } else if (r == n) {
+        //     return;
+        // } else if (l == r) {
+        //     v = v + "(";
+        //     dfs(n, v, l+1, r);
+        // } else {
+        //     auto tmp = v;
+
+        //     v = tmp;
+        //     v = v + "(";
+        //     dfs(n, v, l+1, r);
+
+        //     v = tmp;
+        //     v = v + ")";
+        //     dfs(n, v, l, r+1);
+        // }
     }
 };
