66de1795cc64c2c37fc9ee6953e94a640f752bf1
sunznx <sunznx@163.com>
2018-08-04 13:32:43 -0500

    update 50.powx-n.cpp

---
+++ 50.powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        
+        if (n == 1) {
+            return x;
+        }
+        
+        if (n < 0) {
+            return 1 / myPow(x, -(n+1)) / x;
+        }
+        
+        if (n % 2 == 0) {
+            return myPow(x * x, n/2);
+        } else {
+            return x * myPow(x * x, n/2);
+        }
+    }
+};
