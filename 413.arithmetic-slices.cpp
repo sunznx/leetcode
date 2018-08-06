1d32dfbf15ba1acf149e7a7caab596fcd238871b
sunznx <sunznx@163.com>
2018-08-05 22:18:19 -0500

    update 413.arithmetic-slices.cpp

---
+++ 413.arithmetic-slices.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+        int len = A.size();
+        if (len <= 2) {
+            return 0;
+        }
+
+        vector<int> arr(len-1, 0);
+        for (int i = 0; i < len-1; i++) {
+            arr[i] = A[i+1] - A[i];
+        }
+
+        int n = 1;
+        int res = 0;
+        for (int i = 1; i < arr.size(); i++) {
+            if (i == arr.size()-1) {
+                if (arr[i] == arr[i-1]) {
+                    n += 1;
+                }
+                res += sumHelp(n);
+            } else {
+                if (arr[i] != arr[i-1]) {
+                    res += sumHelp(n);
+                    n = 1;
+                }
+                if (arr[i] == arr[i-1]) {
+                    n += 1;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    int sumHelp(int n) {
+        return n * (n-1) / 2;
+    }
+};
