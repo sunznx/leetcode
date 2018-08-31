21c32b877881e3f5ecf686237cd5ccff736982b5
sunznx <sunznx@163.com>
2018-08-31 03:04:37 -0500

    update 860.lemonade-change.cpp

---
+++ 860.lemonade-change.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int num_5 = 0;
+        int num_10 = 0;
+        int num_20 = 0;
+
+        int len = bills.size();
+        for (int i = 0; i < len; i++) {
+            if (bills[i] == 5) {
+                num_5 += 1;
+            } else if (bills[i] == 10) {
+                if (num_5 == 0) {
+                    return false;
+                }
+                num_5 -= 1;
+                num_10 += 1;
+            } else if (bills[i] == 20) {
+                if (num_10 >= 1 && num_5 >= 1) {
+                    num_5 -= 1;
+                    num_10 -= 1;
+                } else if (num_10 == 0 && num_5 >= 3) {
+                    num_5 -= 3;
+                } else {
+                    return false;
+                }
+
+                num_20 += 1;
+            }
+        }
+
+        return true;
+    }
+};
