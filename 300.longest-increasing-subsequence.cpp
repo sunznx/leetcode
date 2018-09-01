865855c6030e30450ef46c84accb62634f81a32c
sunznx <sunznx@163.com>
2018-09-01 01:16:22 -0500

    update 300.longest-increasing-subsequence.cpp

--- 300.longest-increasing-subsequence.cpp
+++ 300.longest-increasing-subsequence.cpp
@@ -3,7 +3,6 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> aux = nums;
         sort(aux.begin(), aux.end());
-        aux.resize(unique(aux.begin(), aux.end()) - aux.begin());
 
         int len1 = nums.size();
         int len2 = aux.size();
@@ -11,7 +10,9 @@ public:
         
         for (int i = 0; i < len1; i++) {
             for (int j = 0; j < len2; j++) {
-                if (nums[i] == aux[j]) {
+                if (j > 0 && aux[j] == aux[j-1]) {
+                    dp[i+1][j+1] = dp[i+1][j];
+                } else if (nums[i] == aux[j]) {
                     dp[i+1][j+1] = dp[i][j] + 1;
                 } else {
                     dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1]);
