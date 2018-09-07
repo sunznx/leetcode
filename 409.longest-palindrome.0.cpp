c388fee12dc168eaaad011347c277740d8cc19b6
sunznx <sunznx@163.com>
2018-09-07 00:23:23 -0500

    update 409.longest-palindrome.0.cpp

---
+++ 409.longest-palindrome.0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        sort(s.begin(), s.end());
+        
+        int ans = 0;
+        int len = s.size();
+        int i = 1;
+        while (i < len) {
+            if (s[i] == s[i-1]) {
+                ans += 2;
+                i += 2;
+            } else {
+                i += 1;
+            }
+        }
+        
+        if (len-ans > 0) {
+            ans += 1;
+        }
+        
+        return ans;
+    }
+};
