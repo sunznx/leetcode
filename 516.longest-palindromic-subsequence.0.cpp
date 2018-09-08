8b262b150b23eef22a700ccae4573b3af6c17c91
sunznx <sunznx@163.com>
2018-09-07 23:52:17 -0500

    update 516.longest-palindromic-subsequence.0.cpp

--- 516.longest-palindromic-subsequence.0.cpp
+++ 516.longest-palindromic-subsequence.0.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int longestPalindromeSubseq(string s) {
         int len = s.size();
-        vector<vector<int>> dp(len+1, vector<int>(len+1, 0));
         
+        vector<vector<int>> dp(len+1, vector<int> (len+1, 0));
         for (int i = len-1; i >= 0; i--) {
             dp[i][i] = 1;
             for (int j = i+1; j < len; j++) {
