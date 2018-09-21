57020f17e0b0dbae352d5941b7252911467ee518
sunznx <sunznx@163.com>
2018-09-21 01:09:10 -0500

    update 690.employee-importance.cpp

---
+++ 690.employee-importance.cpp
@@ -0,0 +1,40 @@
+/*
+// Employee info
+class Employee {
+public:
+    // It's the unique ID of each node.
+    // unique id of this employee
+    int id;
+    // the importance value of this employee
+    int importance;
+    // the id of direct subordinates
+    vector<int> subordinates;
+};
+*/
+class Solution {
+public:
+    map<int, Employee*> m;
+    map<int, int> counted;
+    int res = 0;
+    int getImportance(vector<Employee*> employees, int id) {
+        for (int i = 0; i < employees.size(); i++) {
+            int eid = employees[i]->id;
+            m[eid] = employees[i];
+        }
+        
+        dfs(id);
+        return res;
+    }
+    
+    void dfs(int id) {
+        if (counted[id]) {
+            return;
+        }
+        
+        res += m[id]->importance;
+        
+        for (int i = 0; i < m[id]->subordinates.size(); i++) {
+            dfs(m[id]->subordinates[i]);
+        }
+    }
+};
