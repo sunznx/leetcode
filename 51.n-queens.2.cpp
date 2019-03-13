581d3a08f8205ec5fb31c6591729570988125754
sunznx <sunznx@163.com>
2019-03-13 22:19:24 +0800

    update 51.n-queens.2.cpp

--- 51.n-queens.2.cpp
+++ 51.n-queens.2.cpp
@@ -5,17 +5,17 @@ public:
     vector<int> c3;
     vector<int> c4;
     vector<string> q;
-    int num;
+    int n;
     int marked;
     vector<vector<string>> res;
 
-    void init(int n) {
+    void init(int num) {
         c1.clear();
         c2.clear();
         c3.clear();
         c4.clear();
         q.clear();
-        num = n;
+        n = num;
         marked = 0;
 
         for (int i = 0; i <= 2*n; i++) {
@@ -32,20 +32,20 @@ public:
 
     vector<vector<string>> solveNQueens(int n) {
         init(n);
-        dfs(0, 0);
+        dfs(0);
         return res;
     }
 
-    void dfs(int x, int y) {
-        if (x == num) {
+    void dfs(int x) {
+        if (x == n) {
             res.push_back(q);
             return;
         }
 
-        for (int j = 0; j < num; j++) {
+        for (int j = 0; j < n; j++) {
             if (check(x, j)) {
                 mark(x, j);
-                dfs(x+1, 0);
+                dfs(x+1);
                 unmark(x, j);
             }
         }
@@ -54,7 +54,7 @@ public:
     void mark(int x, int y) {
         c1[x] = 1;
         c2[y] = 1;
-        c3[y-x+num] = 1;
+        c3[y-x+n] = 1;
         c4[x+y] = 1;
         q[x][y] = 'Q';
         marked += 1;
@@ -63,13 +63,13 @@ public:
     void unmark(int x, int y) {
         c1[x] = 0;
         c2[y] = 0;
-        c3[y-x+num] = 0;
+        c3[y-x+n] = 0;
         c4[x+y] = 0;
         q[x][y] = '.';
         marked -= 1;
     }
 
     bool check(int x, int y) {
-        return c1[x] == 0 && c2[y] == 0 && c3[y-x+num] == 0 && c4[x+y] == 0;
+        return c1[x] == 0 && c2[y] == 0 && c3[y-x+n] == 0 && c4[x+y] == 0;
     }
 };
