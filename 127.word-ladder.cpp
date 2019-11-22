1a8cf2c63a8c576fc57cd3239452344aa7fd978c
sunznx <sunznx@163.com>
2019-11-22 17:08:36 +0800

    update 127.word-ladder.cpp

--- 127.word-ladder.cpp
+++ 127.word-ladder.cpp
@@ -30,6 +30,10 @@ public:
             s.insert(wordList[i]);
         }
 
+        if (s.find(endWord) == s.end()) {
+            return 0;
+        }
+
         q.push(beginWord);
         m[beginWord] = 1;
 
