033aeecabb5e48686279001091ce1cea0149eb27
sunznx <sunznx@163.com>
2019-11-23 10:02:47 +0800

    update 55.jump-game.cpp

--- 55.jump-game.cpp
+++ 55.jump-game.cpp
@@ -3,8 +3,7 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int leaves = 0;
-        int len = nums.size();
-        for (int i = 0; i < len-1; i++) {
+        for (int i = 0; i < nums.size()-1; i++) {
             leaves = max(nums[i], leaves-1);
             if (leaves <= 0) {
                 return false;
