8364cb387ca4da5d45d1e8e8c42372093ff4889f
sunznx <sunznx@163.com>
2019-11-27 07:15:01 +0800

    update 326.power-of-three.cpp

---
+++ 326.power-of-three.cpp
@@ -0,0 +1,16 @@
+// CreateTime: 2019-11-26 23:06:09
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        unordered_map<int, int> m;
+        m[1] = 0;
+        
+        int v = 1;
+        for (int i = 1; i <= 19; i++) {
+            m[v*3] = i;
+            v = v * 3;
+        }
+        
+        return m.find(n) != m.end();
+    }
+};
