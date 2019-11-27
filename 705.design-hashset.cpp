0d5abbfb71772f25d4d58680ae0185274ecf7574
sunznx <sunznx@163.com>
2019-11-27 10:04:57 +0800

    update 705.design-hashset.cpp

---
+++ 705.design-hashset.cpp
@@ -0,0 +1,46 @@
+// CreateTime: 2019-11-27 09:57:58
+class MyHashSet {
+public:
+    vector<list<int>> h;
+    const int MOD = 0x3fffff;
+
+    /** Initialize your data structure here. */
+    MyHashSet() {
+        h.resize(MOD);
+    }
+    
+    void add(int key) {
+        int mod = key % MOD;
+        for (auto item: h[mod]) {
+            if (item == key) {
+                return;
+            }
+        }
+
+        h[mod].push_back(key);
+    }
+    
+    void remove(int key) {
+        int mod = key % MOD;
+        h[mod].remove(key);
+    }
+    
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+        int mod = key % MOD;
+        for (auto item: h[mod]) {
+            if (item == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
