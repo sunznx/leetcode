12ab9b44677ec13a7178b6694f1b405b34a316a6
sunznx <sunznx@163.com>
2019-11-28 11:09:12 +0800

    update 447.number-of-boomerangs.cpp

--- 447.number-of-boomerangs.cpp
+++ 447.number-of-boomerangs.cpp
@@ -1,31 +1,26 @@
 // CreateTime: 2019-11-28 10:18:13
 class Solution {
 public:
-
-    struct Point {
-        int x;
-        int y;
-
-        int distance(Point &p) {
-            return (x-p.x)*(x-p.x) + (y-p.y)*(y-p.y);
-        }
-    };
-
     int numberOfBoomerangs(vector<vector<int>>& points) {
         int len = points.size();
 
-        vector<Point> p(len);
+        vector<pair<int, int>> p(len);
 
         vector<unordered_map<int, int>> distance(len);
 
         for (int i = 0; i < len; i++) {
-            p[i].x = points[i][0];
-            p[i].y = points[i][1];
+            p[i].first = points[i][0];
+            p[i].second = points[i][1];
         }
 
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < len; j++) {
-                int d = p[i].distance(p[j]);
+                if (i == j) {
+                    continue;
+                }
+                int x = (p[i].first - p[j].first);
+                int y = (p[i].second - p[j].second);
+                int d = x*x+y*y;
                 distance[i][d] += 1;
             }
         }
