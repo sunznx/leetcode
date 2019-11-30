17e4f2e05cc5f3d40a03df366c5b8fe5846e94a5
sunznx <sunznx@163.com>
2019-12-01 07:08:43 +0800

    update 4.median-of-two-sorted-arrays.cpp

--- 4.median-of-two-sorted-arrays.cpp
+++ 4.median-of-two-sorted-arrays.cpp
@@ -8,14 +8,14 @@ public:
         int len = (len1 + len2);
         if (len % 2 == 1) {
             int k = len/2;
-            return findK1(nums1, nums2, k);
+            return (double)findK(nums1, nums2, k);
         }
-
-        int k = len/2-1;
-        return findK2(nums1, nums2, k);
+        int k1 = len/2;
+        int k2 = len/2-1;
+        return (double) (findK(nums1, nums2, k1) + findK(nums1, nums2, k2)) / 2;
     }
 
-    double findK1(vector<int>& nums1, vector<int>& nums2, int k) {
+    int findK(vector<int>& nums1, vector<int>& nums2, int k) {
         int len1 = nums1.size();
         int len2 = nums2.size();
         int i = 0;
@@ -23,7 +23,7 @@ public:
 
         int a;
         int b;
-        double res;
+        int res;
 
         for (int t = 0; t <= k; t++) {
             if (i == len1) {
@@ -46,47 +46,5 @@ public:
             }
         }
         return res;
-    }
-
-    double findK2(vector<int>& nums1, vector<int>& nums2, int k) {
-        int len1 = nums1.size();
-        int len2 = nums2.size();
-        int i = 0;
-        int j = 0;
-
-        int a;
-        int b;
-        int res;
-        int res1;
-        int res2;
-
-        for (int t = 0; t <= k+1; t++) {
-            if (i == len1) {
-                a = 0x3fffffff;
-            } else {
-                a = nums1[i];
-            }
-            if (j == len2) {
-                b = 0x3fffffff;
-            } else {
-                b = nums2[j];
-            }
-
-            if (a < b) {
-                res = a;
-                i++;
-            } else {
-                res = b;
-                j++;
-            }
-
-            if (t == k) {
-                res1 = res;
-            }
-            if (t == k+1) {
-                res2 = res;
-            }
-        }
-        return (double)(res1 + res2) / 2;
     }
 };
