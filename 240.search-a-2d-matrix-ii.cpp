84a40cfee6875b7aab2b617c3b32665e8e5d3f3e
sunznx <sunznx@163.com>
2019-11-30 16:03:54 +0800

    update 240.search-a-2d-matrix-ii.cpp

--- 240.search-a-2d-matrix-ii.cpp
+++ 240.search-a-2d-matrix-ii.cpp
@@ -1,24 +1,3 @@
-// // CreateTime: 2019-11-30 01:08:36
-// #include <iostream>
-// #include <cstdio>
-// #include <cstdlib>
-// #include <cstring>
-// #include <string>
-// #include <sstream>
-// #include <cmath>
-// #include <algorithm>
-// #include <queue>
-// #include <deque>
-// #include <vector>
-// #include <list>
-// #include <stack>
-// #include <map>
-// #include <set>
-// #include <unordered_set>
-// #include <unordered_map>
-
-// using namespace std;
-
 // CreateTime: 2019-11-29 11:52:58
 class Solution {
 public:
@@ -80,6 +59,7 @@ public:
                     l = mid+1;
                 } else {
                     r = mid;
+                    maxr = min(maxr, mid);
                 }
             }
 
@@ -91,28 +71,3 @@ public:
         return false;
     }
 };
-
-// int main(void) {
-//     // vector<vector<int>> matrix = {
-//     //     {1,4,7,11,15},
-//     //     {2,5,8,12,19},
-//     //     {3,6,9,16,22},
-//     //     {10,13,14,17,24},
-//     //     {18,21,23,26,30}
-//     // };
-//     // int target = 13;
-
-//     vector<vector<int>> matrix = {
-//         {1,3,5,7,9},
-//         {2,4,6,8,10},
-//         {11,13,15,17,19},
-//         {12,14,16,18,20},
-//         {21,22,23,24,25}
-//     };
-//     int target = 24;
-
-//     Solution s;
-//     cout << s.searchMatrix(matrix, target) << endl;
-
-//     return 0;
-// }
