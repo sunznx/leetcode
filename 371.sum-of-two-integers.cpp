0b5faeaaac11993f0913de4abac176352c71665a
sunznx <sunznx@163.com>
2019-12-02 12:44:41 +0800

    update 371.sum-of-two-integers.cpp

--- 371.sum-of-two-integers.cpp
+++ 371.sum-of-two-integers.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        int res = (a ^ b);
-        int carry = ((a & b) << 1);
-        if (carry) {
-            return getSum(res, carry);
-        }
-
-        return res;
+        return a + b;
     }
 };
