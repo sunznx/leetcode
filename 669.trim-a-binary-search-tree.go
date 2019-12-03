e1c445b256a0e3f671ad7baf620be6093092b5bd
sunx <sunznx@163.com>
2019-12-03 08:02:50 +0800

    update 669.trim-a-binary-search-tree.go

--- 669.trim-a-binary-search-tree.go
+++ 669.trim-a-binary-search-tree.go
@@ -17,6 +17,7 @@ func trimBST(root *TreeNode, L int, R int) *TreeNode {
 	if root.Val > R {
 		return trimBST(root.Left, L, R)
 	}
+
 	root.Left = trimBST(root.Left, L, R)
 	root.Right = trimBST(root.Right, L, R)
 	return root
