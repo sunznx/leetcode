c7fc54240b5688c3e5009e5bf781bd73713054de
sunx <sunznx@163.com>
2019-12-03 08:01:50 +0800

    update 413.arithmetic-slices.cpp

--- 413.arithmetic-slices.cpp
+++ 413.arithmetic-slices.cpp
@@ -14,19 +14,13 @@ public:
         int n = 1;
         int res = 0;
         for (int i = 1; i < arr.size(); i++) {
-            if (i == arr.size()-1) {
-                if (arr[i] == arr[i-1]) {
-                    n += 1;
-                }
+            if (arr[i] == arr[i-1]) {
+                n += 1;
+            }
+
+            if (i == arr.size()-1 || arr[i] != arr[i-1]) {
                 res += sumHelp(n);
-            } else {
-                if (arr[i] != arr[i-1]) {
-                    res += sumHelp(n);
-                    n = 1;
-                }
-                if (arr[i] == arr[i-1]) {
-                    n += 1;
-                }
+                n = 1;
             }
         }
 
