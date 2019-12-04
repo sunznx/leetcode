a2d03f1539a0438ca118426e7b0b87b4c8603080
sunznx <sunznx@163.com>
2019-12-04 11:15:03 +0800

    update 456.132-pattern.cpp

---
+++ 456.132-pattern.cpp
@@ -0,0 +1,28 @@
+// CreateTime: 2019-11-30 18:15:46
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int len = nums.size();
+        if (len == 0) {
+            return false;
+        }
+        
+        int k = -0x7fffffff;
+        stack<int> stk;
+
+        for (int i = len-1; i >= 0; i--) {
+            if (nums[i] < k) {
+                return true;
+            }
+
+            while (!stk.empty() && stk.top() < nums[i]) {
+                k = stk.top();
+                stk.pop();
+            }
+            stk.push(nums[i]);
+        }
+        
+        return false;
+    }
+};
+
