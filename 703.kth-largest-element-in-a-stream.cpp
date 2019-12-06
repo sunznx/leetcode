5ddcacf39651fd5bc1e8e11d78171400d7db4a26
sunznx <sunznx@163.com>
2019-12-06 11:55:54 +0800

    update 703.kth-largest-element-in-a-stream.cpp

---
+++ 703.kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,41 @@
+// CreateTime: 2019-12-06 11:40:38
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> q;
+    int K;
+
+    KthLargest(int k, vector<int>& nums) {
+        K = k;
+
+        int len = nums.size();
+        for (int i = 0; i < len; i++) {
+            if (q.size() == k) {
+                if (nums[i] > q.top()) {
+                    q.pop();
+                    q.push(nums[i]);
+                }
+            } else {
+                q.push(nums[i]);
+            }
+        }
+    }
+
+    int add(int val) {
+        if (q.size() == K) {
+            if (val > q.top()) {
+                q.pop();
+                q.push(val);
+            }
+        } else {
+            q.push(val);
+        }
+
+        return q.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
