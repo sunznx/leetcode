40dab875ad82a8314124239f58559aa4cd9784f8
sunznx <sunznx@163.com>
2019-12-11 11:43:07 +0800

    update 30.substring-with-concatenation-of-all-words.0.cpp

---
+++ 30.substring-with-concatenation-of-all-words.0.cpp
@@ -0,0 +1,44 @@
+// CreateTime: 2019-12-11 11:15:35
+class Solution {
+public:
+    unordered_set<string> se;
+
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> res;
+        if (words.size() == 0 || words[0].size() == 0) {
+            return res;
+        }
+
+        unordered_map<string, int> m;
+        for (auto &word: words) {
+            m[word] += 1;
+        }
+
+        int len = s.size();
+        int e = words[0].size();
+        int k = words.size();
+
+        for (int i = 0; i <= len-k*e; i++) {
+            if (check(s, m, i, e, k)) {
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+
+    bool check(string &s, unordered_map<string, int> &m, int idx, int e, int k) {
+        unordered_map<string, int> c;
+        int eq = 0;
+
+        for (int i = 0; i < k; i++) {
+            string sub = s.substr(idx+i*e, e);
+
+            c[sub] += 1;
+            if (m.find(sub) != m.end() && c[sub] == m[sub]) {
+                eq += 1;
+            }
+        }
+
+        return eq == m.size();
+    }
+};
