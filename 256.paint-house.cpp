d62b209840f33bed2c1bc00d7d113df5bb6fa015
sunznx <sunznx@163.com>
2019-12-12 10:59:01 +0800

    update 256.paint-house.cpp

--- 256.paint-house.cpp
+++ 256.paint-house.cpp
@@ -1,18 +1,21 @@
 // CreateTime: 2019-12-11 17:49:17
 class Solution {
 public:
-    int ans = 0x7fffffff;
-
     int minCost(vector<vector<int>>& costs) {
-        vector<vector<int>> minCosts(costs.size(), vector<int>(3, ans));
+        int len = costs.size();
+        if (len == 0) {
+            return 0;
+        }
+        
+        vector<vector<int>> minCosts(len, vector<int>(3, 0x7fffffff));
         dfs(0, costs, minCosts, 0);
-        return ans;
+        auto x = minCosts[len-1];
+        return min(min(x[0], x[1]), x[2]);
     }
 
     void dfs(int step, vector<vector<int>>& costs, vector<vector<int>> &minCosts, int cost, int pre = -1) {
         int len = costs.size();
         if (step == len) {
-            ans = min(ans, cost);
             return;
         }
 
