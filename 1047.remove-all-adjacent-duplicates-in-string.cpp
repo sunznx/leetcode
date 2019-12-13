7ce5d0e651cb060fc7185dd0d935be7b795cf07e
sunx <sunznx@163.com>
2019-12-14 07:18:54 +0800

    update 1047.remove-all-adjacent-duplicates-in-string.cpp

---
+++ 1047.remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,17 @@
+// CreateTime: 2019-12-14 07:06:28
+class Solution {
+public:
+    string removeDuplicates(string S) {
+        int k = 0;
+        while (k < S.size()) {
+            if (k+1 < S.size() && S[k] == S[k+1]) {
+                S.erase(S.begin()+k+1);
+                S.erase(S.begin()+k);
+                k = max(0, k-1);
+            } else {
+                k++;
+            }
+        }
+        return S;
+    }
+};
