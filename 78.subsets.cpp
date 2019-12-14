ec4d9f0dcee5fb01e833444a6285846954db3049
sunx <sunznx@163.com>
2019-12-15 00:12:20 +0800

    update 78.subsets.cpp

--- 78.subsets.cpp
+++ 78.subsets.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        
         vector<vector<int>> res;
+
         int len = nums.size();
         for (int i = 0; i < (1<<len); i++) {
             vector<int> v;
             for (int j = 0; j < len; j++) {
-                if (((1 << j) & i) ) {
+                if ((i & (1 << j))) {
                     v.push_back(nums[j]);
                 }
             }
             res.push_back(v);
         }
-        
         return res;
     }
 };
