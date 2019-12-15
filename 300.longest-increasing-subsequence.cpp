e99b79147066b219d44c8bdf8023883752212563
sunx <sunznx@163.com>
2019-12-15 10:02:00 +0800

    update 300.longest-increasing-subsequence.cpp

--- 300.longest-increasing-subsequence.cpp
+++ 300.longest-increasing-subsequence.cpp
@@ -1,25 +1,30 @@
+// CreateTime: 2019-12-15 09:46:17
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> aux = nums;
-        sort(aux.begin(), aux.end());
-
-        int len1 = nums.size();
-        int len2 = aux.size();
-        vector<vector<int>> dp(len1+1, vector<int>(len2+1, 0));
+        int len = nums.size();
         
-        for (int i = 0; i < len1; i++) {
-            for (int j = 0; j < len2; j++) {
-                if (j > 0 && aux[j] == aux[j-1]) {
-                    dp[i+1][j+1] = dp[i+1][j];
-                } else if (nums[i] == aux[j]) {
-                    dp[i+1][j+1] = dp[i][j] + 1;
-                } else {
-                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1]);
+        vector<int> m;
+        
+        for (int i = 0; i < len; i++) {
+            int l = 0;
+            int r = m.size()-1;
+            while (l < r) {
+                int mid = (l+r) / 2;
+                if (m[mid] < nums[i]) {
+                    l = mid+1;
+                } else if (m[mid] >= nums[i]) {
+                    r = mid;
                 }
+            }
+
+            if (l >= m.size() || nums[i] > m[m.size()-1]) {
+                m.push_back(nums[i]);
+            } else if (nums[i] < m[l]) {
+                m[l] = nums[i];
             }
         }
         
-        return dp[len1][len2];
+        return m.size();
     }
 };
