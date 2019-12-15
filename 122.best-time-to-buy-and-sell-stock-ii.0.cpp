83348ebcf0d62b2eb0d8578a8c6ca0b150777cfa
sunx <sunznx@163.com>
2019-12-15 10:21:02 +0800

    update 122.best-time-to-buy-and-sell-stock-ii.0.cpp

---
+++ 122.best-time-to-buy-and-sell-stock-ii.0.cpp
@@ -0,0 +1,21 @@
+// CreateTime: 2019-12-15 10:12:54
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int len = prices.size();
+        if (len == 0) {
+            return 0;
+        }
+
+        vector<int> dp(len, 0);
+        
+        for (int i = 1; i < len; i++) {
+            if (prices[i] > prices[i-1]) {
+                dp[i] = dp[i-1] + prices[i]-prices[i-1];
+            } else {
+                dp[i] = dp[i-1];
+            }
+        }
+        return dp[len-1];
+    }
+};
