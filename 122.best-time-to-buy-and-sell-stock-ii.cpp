cd20a9c1b599b80bc3c86bcd5d86b2a10dc94f24
sunx <sunznx@163.com>
2019-12-15 10:22:50 +0800

    update 122.best-time-to-buy-and-sell-stock-ii.cpp

--- 122.best-time-to-buy-and-sell-stock-ii.cpp
+++ 122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,21 @@
+// CreateTime: 2019-12-15 10:12:54
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int sum = 0;
-        for (int i = 1; i < n; i++) {
-            int num = prices[i] - prices[i-1];
-            if (num > 0) {
-                sum += num;
+        int len = prices.size();
+        if (len == 0) {
+            return 0;
+        }
+
+        vector<int> dp(len);
+        
+        for (int i = 1; i < len; i++) {
+            if (prices[i] > prices[i-1]) {
+                dp[i] = dp[i-1] + prices[i]-prices[i-1];
+            } else {
+                dp[i] = dp[i-1];
             }
         }
-        return sum;
+        return dp[len-1];
     }
 };
