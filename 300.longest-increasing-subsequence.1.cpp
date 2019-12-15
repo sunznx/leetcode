bf2662fb3897c51cdf45f0db7d7b4e38875103d8
sunx <sunznx@163.com>
2019-12-15 10:03:06 +0800

    update 300.longest-increasing-subsequence.cpp

--- 300.longest-increasing-subsequence.1.cpp
+++ 300.longest-increasing-subsequence.1.cpp
@@ -1,26 +1,20 @@
+// CreateTime: 2019-12-15 09:40:02
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int len = nums.size();
-        vector<int> aux(len+1, 0x3fffffff);
+        vector<int> dp(len, 1);
+
         int ans = 0;
 
         for (int i = 0; i < len; i++) {
-            int l = 1;
-            int r = ans + 1;
-            while (l < r) {
-                int mid = (l+r) / 2;
-                if (aux[mid] >= nums[i]) {
-                    r = mid;
-                } else {
-                    l = mid+1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
                 }
             }
-            
-            aux[l] = min(aux[l], nums[i]);
-            ans = max(ans, l);
+            ans = max(ans, dp[i]);
         }
-
         return ans;
     }
 };
