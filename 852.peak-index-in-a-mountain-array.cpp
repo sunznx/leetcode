12f8a60c1bc7bf9e15330e825faea44c89c92cdd
sunznx <sunznx@163.com>
2019-12-16 12:33:38 +0800

    update 852.peak-index-in-a-mountain-array.cpp

--- 852.peak-index-in-a-mountain-array.cpp
+++ 852.peak-index-in-a-mountain-array.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& A) {
         for (int i = 0; i < A.size(); i++) {
-            if (A[i] > A[i+1] || i == A.size()-1) {
+            if (i == A.size()-1 || A[i] > A[i+1]) {
                 return i;
             }
         }
+        return -1;
     }
 };
