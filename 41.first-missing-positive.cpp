babe693e790062eaf10033bca1137e867ad8f87b
sunx <sunznx@163.com>
2020-01-04 21:34:46 +0800

    update 41.first-missing-positive.cpp

---
+++ 41.first-missing-positive.cpp
@@ -0,0 +1,19 @@
+// CreateTime: 2020-01-04 21:25:48
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        unordered_set<int> m;
+        
+        int len = nums.size();
+        for (int i = 0; i < len; i++) {
+            m.insert(nums[i]);
+        }
+        
+        for (int i = 1; i <= len; i++) {
+            if (m.find(i) == m.end()) {
+                return i;
+            }
+        }
+        return len+1;
+    }
+};
