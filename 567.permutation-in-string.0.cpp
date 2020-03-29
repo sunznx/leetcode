61044624fd5d1afe9af16da343843005c05cd073
sunznx <sunznx@163.com>
2020-03-29 19:39:58 +0800

    update 567.permutation-in-string.0.cpp

--- 567.permutation-in-string.0.cpp
+++ 567.permutation-in-string.0.cpp
@@ -1,19 +1,22 @@
 // CreateTime: 2020-03-29 18:03:23
 class Solution {
 public:
-    vector<int> m;
+    vector<int> m1;
+    vector<int> m2;
     bool checkInclusion(string s1, string s2) {
+
         auto l1 = s1.size();
         auto l2 = s2.size();
         if (l1 > l2) {
             return false;
         }
 
-        m.resize(26, 0);
+        m1.resize(26, 0);
+        m2.resize(26, 0);
 
         for (int i = 0; i < s1.size(); i++) {
-            m[s1[i]-'a']++;
-            m[s2[i]-'a']--;
+            m1[s1[i]-'a']++;
+            m2[s2[i]-'a']++;
         }
 
         if (check()) {
@@ -21,8 +24,8 @@ public:
         }
 
         for (int i = l1; i < s2.size(); i++) {
-            m[s2[i]-'a']--;
-            m[s2[i-l1]-'a']++;
+            m2[s2[i]-'a']++;
+            m2[s2[i-l1]-'a']--;
             if (check()) {
                 return true;
             }
@@ -32,8 +35,8 @@ public:
     }
 
     bool check() {
-        for (auto &c: m) {
-            if (c != 0) {
+        for (int i = 0; i < 26; i++) {
+            if (m1[i] != m2[i]) {
                 return false;
             }
         }
