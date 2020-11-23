ea8457acd6a55e060ed133af35a754215a2fa89a
sunznx <sunznx@163.com>
2020-11-23 08:41:18 +0800

    update 424.longest-repeating-character-replacement.cpp

--- 424.longest-repeating-character-replacement.cpp
+++ 424.longest-repeating-character-replacement.cpp
@@ -2,35 +2,34 @@
 class Solution
 {
 public:
-    int characterReplacement(string s, int k)
-        {
-            int len = s.size();
-            if (len == 0) {
-                return 0;
-            }
-
-            int left = 0;
-            int right = 0;
-
-            vector<int> m(256);
-            int maxCharCount = 0;
-
-            int ans = 0;
-
-            while (right < len) {
-                char c = s[right++];
-                m[c]++;
-
-                maxCharCount = max(maxCharCount, m[c]);
-
-                if (right-left - maxCharCount > k) {
-                    char d = s[left++];
-                    m[d]--;
-                }
-
-                ans = max(ans, right-left);
-            }
-
-            return ans;
+    int characterReplacement(string s, int k) {
+        int len = s.size();
+        if (len == 0) {
+            return 0;
         }
+
+        int left = 0;
+        int right = 0;
+
+        vector<int> m(256);
+        int maxCharCount = 0;
+
+        int ans = 0;
+
+        while (right < len) {
+            char c = s[right++];
+            m[c]++;
+
+            maxCharCount = max(maxCharCount, m[c]);
+
+            if (right-left - maxCharCount > k) {
+                char d = s[left++];
+                m[d]--;
+            }
+
+            ans = max(ans, right-left);
+        }
+
+        return ans;
+    }
 };
