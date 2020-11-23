57e8dd765a8e924da81d79d53ace918bba1bf64f
sunznx <sunznx@163.com>
2020-11-23 08:08:14 +0800

    update 424.longest-repeating-character-replacement.cpp

---
+++ 424.longest-repeating-character-replacement.cpp
@@ -0,0 +1,35 @@
+// CreateTime: 2020-11-22 03:04:03
+class Solution
+{
+public:
+    int characterReplacement(string s, int k)
+        {
+            int len = s.size();
+            if (len == 0) {
+                return 0;
+            }
+
+            int left = 0;
+            int right = 0;
+
+            unordered_map<char, int> m;
+            int maxCharCount = 0;
+
+            int ans = 0;
+
+            while (right < len) {
+                char c = s[right++];
+                m[c]++;
+                maxCharCount = max(maxCharCount, m[c]);
+
+                if (right-left - maxCharCount > k) {
+                    char d = s[left++];
+                    m[d]--;
+                }
+
+                ans = max(ans, right-left);
+            }
+
+            return ans;
+        }
+};
