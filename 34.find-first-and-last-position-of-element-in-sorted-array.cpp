3867490f72b06cf0eb64988e023d3cd08b919d35
sunznx <sunznx@163.com>
2020-12-01 07:40:58 +0800

    update 34.find-first-and-last-position-of-element-in-sorted-array.cpp

--- 34.find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,58 @@
+// CreateTime: 2020-12-01 07:34:52
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> res(2, -1);
-        int len = nums.size();
-        if (len == 0) {
-            return res;
+        vector<int> ans(2, -1);
+        ans[0] = findL(nums, target);
+        ans[1] = findR(nums, target);
+        return ans;
+    }
+
+    int findL(vector<int>& nums, int target) {
+        if (nums.size() == 0) {
+            return -1;
         }
 
         int l = 0;
         int r = nums.size()-1;
-        while (l <= r) {
+
+        while (l < r) {
             int m = (l+r) / 2;
-            if (nums[m] == target) {
 
-                int x = m;
-                int y = m;
-
-                while (x >= 0 && nums[x] == target) {
-                    x -= 1;
-                };
-                while (y <= nums.size()-1 && nums[y] == target) {
-                    y += 1;
-                }
-                res[0] = x+1;
-                res[1] = y-1;
-                break;
-            } else if (nums[m] > target) {
-                r = m-1;
-            } else {
+            if (nums[m] < target) {
                 l = m+1;
+            } else if (nums[m] >= target) {
+                r = m;
             }
         }
 
-        return res;
+        if (nums[l] == target) {
+            return l;
+        }
+        return -1;
+    }
+
+    int findR(vector<int>& nums, int target) {
+        if (nums.size() == 0) {
+            return -1;
+        }
+
+        int l = 0;
+        int r = nums.size()-1;
+
+        while (l < r) {
+            int m = (l+r+1) / 2;
+
+            if (nums[m] <= target) {
+                l = m;
+            } else if (nums[m] > target) {
+                r = m-1;
+            }
+        }
+
+        if (nums[l] == target) {
+            return l;
+        }
+        return -1;
     }
 };
