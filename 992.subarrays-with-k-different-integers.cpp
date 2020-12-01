35fd84953cc9b00602254bcb62aa5a784c58a37f
sunznx <sunznx@163.com>
2020-12-01 08:56:11 +0800

    update 992.subarrays-with-k-different-integers.cpp

---
+++ 992.subarrays-with-k-different-integers.cpp
@@ -0,0 +1,37 @@
+// CreateTime: 2020-12-01 07:45:55
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& A, int K) {
+        return numberOfNotMoreThan(A, K) - numberOfNotMoreThan(A, K-1);
+    }
+
+    int numberOfNotMoreThan(vector<int> &A, int K) {
+        int ans = 0;
+
+        int l = 0;
+        int r = 0;
+
+        int len = A.size();
+
+        unordered_map<int, int> window;
+
+        int prefix = 0;
+
+        while (r < len) {
+            int x = A[r++];
+            window[x]++;
+
+            while (window.size() > K) {
+                auto y = A[l++];
+                window[y]--;
+                if (window[y] == 0) {
+                    window.erase(y);
+                }
+            }
+
+            ans += r-l+1;
+        }
+
+        return ans;
+    }
+};
