428215bcb6ac783caf53f987f0db02c4c5f18cf7
sunznx <sunznx@163.com>
2020-12-02 18:40:58 +0800

    update 321.create-maximum-number.cpp

---
+++ 321.create-maximum-number.cpp
@@ -0,0 +1,71 @@
+// CreateTime: 2020-12-02 10:27:50
+class Solution
+{
+public:
+    vector<int> maxNumber(vector<int> &nums1, vector<int> &nums2, int k)
+    {
+        vector<int> ans(k, -1);
+        vector<int> m(k, -1);
+
+        for (int i = 0; i <= k; i++) {
+            auto l1 = i;
+            auto l2 = k - i;
+            if (nums1.size() >= l1 && nums2.size() >= l2) {
+                auto max1 = maxK(nums1, l1);
+                auto max2 = maxK(nums2, l2);
+                merge(max1, max2, m);
+                ans = max(ans, m);
+            }
+        }
+        return ans;
+    }
+
+    void merge(vector<int> &nums1, vector<int> &nums2, vector<int> &m, int k = 0)
+    {
+        if (nums1.size() == 0 && nums2.size() == 0) {
+            return;
+        }
+
+        int l1 = 0;
+        int l2 = 0;
+
+        if (nums1.size() == 0) {
+            m[k++] = nums2[l2++];
+        } else if (nums2.size() == 0) {
+            m[k++] = nums1[l1++];
+        } else if (nums1 > nums2) {
+            m[k++] = nums1[l1++];
+        } else {
+            m[k++] = nums2[l2++];
+        }
+
+        auto newNums1 = vector<int>(nums1.begin()+l1, nums1.end());
+        auto newNums2 = vector<int>(nums2.begin()+l2, nums2.end());
+        merge(newNums1, newNums2, m, k);
+    }
+
+    vector<int> maxK(vector<int> &nums, int k)
+    {
+        int len = nums.size();
+
+        int maxRemove = len - k;
+        int removed = 0;
+
+        deque<int> q;
+
+        for (int i = 0; i < len; i++) {
+            while (removed < maxRemove && q.size() && nums[i] > q.back()) {
+                removed++;
+                q.pop_back();
+            }
+            q.push_back(nums[i]);
+        }
+
+        vector<int> ans(k);
+        for (int i = 0; i < k; i++) {
+            ans[i] = q.front();
+            q.pop_front();
+        }
+        return ans;
+    }
+};
