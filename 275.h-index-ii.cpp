dfc5d3cb33a33171daaf9e6d57676be0d01f6c1c
sunznx <sunznx@163.com>
2020-12-02 21:09:39 +0800

    update 275.h-index-ii.cpp

--- 275.h-index-ii.cpp
+++ 275.h-index-ii.cpp
@@ -12,14 +12,14 @@ public:
 
         while (l < r) {
             int m = (l+r+1) / 2;
-            if (citations[len-1-m] > m) {
+            if (citations[len-1-m] >= m+1) {
                 l = m;
             } else if (citations[len-1-m] <= m) {
                 r = m-1;
             }
         }
 
-        if (citations[len-1-l] > l) {
+        if (citations[len-1-l] >= l+1) {
             return l+1;
         }
         return 0;
