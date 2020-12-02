7e740ec95d0fd94e72a89b8f564f01e6f16a3ff6
sunznx <sunznx@163.com>
2020-12-02 21:00:42 +0800

    update 275.h-index-ii.cpp

--- 275.h-index-ii.cpp
+++ 275.h-index-ii.cpp
@@ -3,14 +3,26 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int len = citations.size();
+        if (len == 0) {
+            return 0;
+        }
 
-        int ans = 0;
-        for (int i = len-1; i >= 0; i--) {
-            if (citations[i] > ans) {
-                ans++;
+        int l = 0;
+        int r = len-1;
+
+        while (l < r) {
+            int m = (l+r+1) / 2;
+
+            if (citations[len-1-m] >= m) {
+                l = m;
+            } else if (citations[len-1-m] < m) {
+                r = m-1;
             }
         }
 
-        return ans;
+        if (citations[len-1-l] >= l+1) {
+            return l+1;
+        }
+        return l;
     }
 };
