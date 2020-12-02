81f0121c699eae40758a20afa06108596fafcd92
sunznx <sunznx@163.com>
2020-12-03 00:24:36 +0800

    update 71.simplify-path.0.cpp

---
+++ 71.simplify-path.0.cpp
@@ -0,0 +1,62 @@
+// CreateTime: 2020-12-02 23:53:12
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> stk;
+
+        string sub = "";
+        for (int i = 0; i < path.size(); i++) {
+            char c = path[i];
+            if (c == '/') {
+                if (sub.size()) {
+                    stk.push(sub);
+                } else {
+                    stk.push("/");
+                }
+                sub = "/";
+                continue;
+            }
+
+            sub.push_back(c);
+
+            if (i == path.size()-1) {
+                stk.push(sub);
+                sub = "";
+            }
+        }
+
+        string res = "";
+        while (stk.size()) {
+            auto sub = getTop(stk);
+            if (sub == "/") {
+                if (res.size() != 0) {
+                    continue;
+                } else {
+                    res = "/";
+                }
+            } else {
+                res = sub + res;
+            }
+        }
+        return res;
+    }
+
+    string getTop(stack<string> &stk) {
+        while (stk.size()) {
+            auto sub = stk.top();
+            stk.pop();
+
+            if (sub == "/" || sub == "/.") {
+                continue;
+            }
+
+            if (sub == "/..") {
+                getTop(stk);
+                return getTop(stk);
+            }
+
+            return sub;
+        }
+        return "/";
+    }
+};
