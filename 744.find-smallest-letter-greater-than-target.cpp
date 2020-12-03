91c6697118cfe2f7d8ac4a60425c9d66da2c0ae0
sunznx <sunznx@163.com>
2020-12-04 07:49:41 +0800

    update 744.find-smallest-letter-greater-than-target.cpp

--- 744.find-smallest-letter-greater-than-target.cpp
+++ 744.find-smallest-letter-greater-than-target.cpp
@@ -1,14 +1,25 @@
+// CreateTime: 2020-12-04 07:38:55
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        int len = letters.size();
+        int l = 0;
+        int r = letters.size()-1;
 
-        for (int i = 0; i < len; i++) {
-            if (letters[i] > target) {
-                return letters[i];
+        while (l < r) {
+            int m = (l+r+1) / 2;
+
+            if (letters[m] > target) {
+                r = m-1;
+            } else if (letters[m] <= target) {
+                l = m;
             }
         }
-        
-        return letters[0];
+
+        if (letters[l] > target) {
+            return letters[l];
+        }
+
+        int k = (l+1) % letters.size();
+        return letters[k];
     }
 };
