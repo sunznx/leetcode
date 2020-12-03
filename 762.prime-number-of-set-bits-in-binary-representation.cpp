14aa723d73dd627dca611f362dfbf249867d349f
sunznx <sunznx@163.com>
2020-12-03 17:08:23 +0800

    update 762.prime-number-of-set-bits-in-binary-representation.cpp

--- 762.prime-number-of-set-bits-in-binary-representation.cpp
+++ 762.prime-number-of-set-bits-in-binary-representation.cpp
@@ -14,7 +14,7 @@ public:
             return false;
         }
 
-        for (int i = 3; i*i <= n; i+=2) {
+        for (int i = 3; i <= n/i; i+=2) {
             if (n % i == 0) {
                 return false;
             }
@@ -38,7 +38,7 @@ public:
         int ans = 0;
         for (int i = l; i <= r; i++) {
             if (isPrime(countBit(i))) {
-                ans += 1;
+                ans++;
             }
         }
         
