56ec00299571ce7a425447096d776c0dcaa6123a
sunznx <sunznx@163.com>
2020-12-04 01:30:44 +0800

    update 659.split-array-into-consecutive-subsequences.0.cpp

--- 659.split-array-into-consecutive-subsequences.0.cpp
+++ 659.split-array-into-consecutive-subsequences.0.cpp
@@ -4,6 +4,12 @@ public:
     bool isPossible(vector<int>& nums) {
         typedef priority_queue<int, vector<int>, greater<int>> pq;
         unordered_map<int, pq> m;
+
+        // m[x] 表示以 x 结尾的队列列表
+        //
+        // m[x] 是一个堆，假设堆的 sie 是 N。表示以 x 结尾的队列有个 N 个
+        // m[x] 的堆顶部是当前所有队列中，长度最小的
+
         for (int i = 0; i < nums.size(); i++) {
             auto x = nums[i];
 
