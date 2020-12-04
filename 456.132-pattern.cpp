9c0843281613174cd5e47b5c1a016fd1ae91aace
sunznx <sunznx@163.com>
2020-12-04 22:49:37 +0800

    update 456.132-pattern.cpp

--- 456.132-pattern.cpp
+++ 456.132-pattern.cpp
@@ -1,28 +1,38 @@
-// CreateTime: 2019-11-30 18:15:46
+// CreateTime: 2020-12-04 08:51:28
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
         int len = nums.size();
-        if (len == 0) {
-            return false;
-        }
-        
-        int k = -0x7fffffff;
-        stack<int> stk;
+        vector<int> minL(len);
+        vector<int> maxR(len);
 
+        for (int i = 0; i < len; i++) {
+            if (i == 0) {
+                minL[i] = nums[i];
+            } else {
+                minL[i] = min(minL[i-1], nums[i]);
+            }
+        }
+
+        deque<int> que;
         for (int i = len-1; i >= 0; i--) {
-            if (nums[i] < k) {
+            auto x = nums[i];
+            
+            maxR[i] = x;
+            while (que.size() && que.front() < x) {
+                maxR[i] = que.front();
+                que.pop_front();
+            }
+            que.push_front(x);
+        }
+
+        for (int i = 0; i < len; i++) {
+            auto x = nums[i];
+            if (minL[i] < maxR[i] && maxR[i] < x) {
                 return true;
             }
-
-            while (!stk.empty() && stk.top() < nums[i]) {
-                k = stk.top();
-                stk.pop();
-            }
-            stk.push(nums[i]);
         }
-        
+
         return false;
     }
 };
-
