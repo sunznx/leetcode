e3d81442fa34fe8a74d4ee43cd17ab54f9f02b17
sunznx <sunznx@163.com>
2020-12-06 23:45:42 +0800

    update 118.pascals-triangle.cpp

--- 118.pascals-triangle.cpp
+++ 118.pascals-triangle.cpp
@@ -1,15 +1,16 @@
+// CreateTime: 2020-12-06 23:44:35
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> res;
         
-        for (int i = 1; i <= numRows; i++) {
-            vector<int> v(i, 0);;
-            for (int j = 1; j <= i; j++) {
-                if (i == 1 || i == j || j == 1) {
-                    v[j-1] = 1;
+        for (int i = 0; i < numRows; i++) {
+            vector<int> v(i+1, 0);
+            for (int j = 0; j <= i; j++) {
+                if (i == 0 || i == j || j == 0) {
+                    v[j] = 1;
                 } else {
-                    v[j-1] = res[i-2][j-2] + res[i-2][j-1];
+                    v[j] = res[i-1][j-1] + res[i-1][j];
                 }
             }
             res.push_back(v);
