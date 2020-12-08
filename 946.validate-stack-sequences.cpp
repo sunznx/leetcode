3b590fd63bf60ae319250e833abdfdc8dd94b1f9
sunznx <sunznx@163.com>
2020-12-08 08:14:38 +0800

    update 946.validate-stack-sequences.cpp

--- 946.validate-stack-sequences.cpp
+++ 946.validate-stack-sequences.cpp
@@ -1,39 +1,19 @@
-// CreateTime: 2019-11-20 20:14:31
+// CreateTime: 2020-12-08 07:54:54
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        int len = pushed.size();
+        stack<int> stk;
+        int k = 0;
+        for (int i = 0; i < pushed.size(); i++) {
+            auto x = pushed[i];
+            stk.push(x);
 
-        unordered_map<int, int> m;
-        vector<bool> v(len);
-
-        stack<int> s;
-
-        for (int i = 0; i < len; i++) {
-            m[pushed[i]] = i;
-        }
-
-        for (int i = 0; i < len; i++) {
-            int curIdx = m[popped[i]];
-
-            if (i != 0) {
-                int preIdx = m[popped[i-1]];
-                if (curIdx < preIdx) {
-                    if (s.empty() || popped[i] != s.top()) {
-                        return false;
-                    } else {
-                        s.pop();
-                    }
-                }
-            }
-
-            v[curIdx] = true;
-            if (curIdx != 0 && !v[curIdx-1]) {
-                s.push(pushed[curIdx-1]);
-                v[curIdx-1] = true;
+            while (stk.size() && stk.top() == popped[k]) {
+                k++;
+                stk.pop();
             }
         }
 
-        return true;
+        return k == popped.size();
     }
 };
