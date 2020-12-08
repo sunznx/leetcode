b83a13711cb62a744ae84ab8d0af16ec0f318204
sunznx <sunznx@163.com>
2020-12-08 15:53:57 +0800

    update 682.baseball-game.cpp

--- 682.baseball-game.cpp
+++ 682.baseball-game.cpp
@@ -1,30 +1,39 @@
+// CreateTime: 2020-12-08 15:46:43
 class Solution {
 public:
     int calPoints(vector<string>& ops) {
-        int res = 0;
-        int len = ops.size();
+        int ans = 0;
         
-        vector<int> arr(len, 0);
-        for (int i = 0; i < len; i++) {
-            if (ops[i] == "D") {
-                arr[i] = arr[i-1] * 2;
-                res += arr[i];
-            } else if (ops[i] == "C") {
-                res -= arr[i-1];
-                for (int j = i; j > 0; j--) {
-                    arr[j] = arr[j-2];
-                }
-                arr[0] = 0;
-                arr[1] = 0;
-            } else if (ops[i] == "+") {
-                arr[i] = arr[i-1] + arr[i-2];
-                res += arr[i];
-            } else {
-                arr[i] = atoi(ops[i].c_str());
-                res += arr[i];
+        stack<int> stk;
+        for (int i = 0; i < ops.size(); i++) {
+            if (ops[i] == "C") {
+                auto top = stk.top();
+                stk.pop();
+                ans -= top;
+            }
+            else if (ops[i] == "D") {
+                auto top = stk.top();
+                stk.push(top * 2);
+                ans += top * 2;
+            }
+            else if (ops[i] == "+") {
+                auto top1 = stk.top();
+                stk.pop();
+                
+                auto top2 = stk.top();
+                stk.pop();
+                
+                stk.push(top2);
+                stk.push(top1);
+                stk.push(top1 + top2);
+                ans += top1 + top2;
+            }
+            else {
+                auto val = stoi(ops[i]);
+                stk.push(val);
+                ans += val;
             }
         }
-
-        return res;
+        return ans;
     }
 };
