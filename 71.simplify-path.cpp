34efed990f93ef0ba22c97e5439140b7f4c7b2fe
sunznx <sunznx@163.com>
2020-12-08 22:59:59 +0800

    update 71.simplify-path.cpp

--- 71.simplify-path.cpp
+++ 71.simplify-path.cpp
@@ -1,39 +1,49 @@
 // CreateTime: 2020-01-02 16:49:56
 class Solution {
 public:
-    string simplifyPath(string path) {
-        int len = path.size();
-
-        stack<string> s;
+    vector<string> split(string &s, char x) {
+        vector<string> ans;
         string sub = "";
-
-        for (int i = 0; i < len; i++) {
-            char c = path[i];
-            if (c != '/') {
-                sub = sub + c;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != x) {
+                sub.push_back(s[i]);
             }
 
-            if (c == '/' || i == len-1) {
-                if (sub == "..") {
-                    if (s.size()) {
-                        s.pop();
-                    }
-                } else if (sub != "" && sub != ".") {
-                    s.push("/" + sub);
-                }
+            if (s[i] == x || i == s.size()-1) {
+                ans.push_back(sub);
                 sub = "";
             }
         }
+        return ans;
+    }
 
-        if (s.size() == 0) {
-            return "/";
+    string simplifyPath(string path) {
+        stack<string> stk;
+        auto pathArr = split(path, '/');
+        for (int i = 0; i < pathArr.size(); i++) {
+            auto x = pathArr[i];
+            if (x == "." || x == "") {
+                continue;
+            } else if (x == "..") {
+                if (stk.size()) {
+                    stk.pop();
+                }
+            } else {
+                stk.push(x);
+            }
         }
 
-        string res = "";
-        while (s.size()) {
-            res = s.top() + res;
-            s.pop();
+        string ans = "";
+        while (stk.size()) {
+            auto x = stk.top();
+            stk.pop();
+
+            if (ans == "") {
+                ans = x;
+            } else {
+                ans = x + "/" + ans;
+            }
         }
-        return res;
+        return "/" + ans;
     }
 };
