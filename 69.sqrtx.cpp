112bc455481e2620035c99aab29c178798a7cf9f
sunznx <sunznx@163.com>
2020-12-10 03:38:58 +0800

    update 69.sqrtx.cpp

--- 69.sqrtx.cpp
+++ 69.sqrtx.cpp
@@ -1,26 +1,19 @@
+// CreateTime: 2020-12-10 03:38:24
 class Solution {
 public:
     int mySqrt(int x) {
-        int L = 1;
-        int R = x;
+        long long l = 0;
+        long long r = x; 
 
-        while (L <= R) {
-            long mid = (R - L) / 2 + L;
-
-            long sum = mid * mid;
-            long sum2 = (mid+1) * (mid+1);
-
-            if (sum == x) {
-                return mid;
-            } else if (sum < x && x < sum2) {
-                return mid;
-            } else if (sum < x) {
-                L = mid + 1;
-            } else if (sum > x) {
-                R = mid - 1;
+        while (l < r) {
+            long long mid = (l+r+1)/2;
+            long long val = mid * mid;
+            if (val <= x) {
+                l = mid;
+            } else if (val > x) {
+                r = mid-1;
             }
         }
-
-        return 0;
+        return (int)l;
     }
 };
