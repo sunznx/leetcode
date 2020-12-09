1421c63d074e1d28d0a5084cf6919c84393959c2
sunznx <sunznx@163.com>
2020-12-10 04:23:27 +0800

    update 198.house-robber.cpp

--- 198.house-robber.cpp
+++ 198.house-robber.cpp
@@ -1,23 +1,22 @@
+// CreateTime: 2020-12-10 04:19:08
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int len = nums.size();
-
         if (len == 0) {
             return 0;
-        } else if (len == 1) {
+        }
+
+        if (len == 1) {
             return nums[0];
         }
 
-        vector<int> dp(len+1, 0);
-
+        vector<int> dp(len);
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
-
         for (int i = 2; i < len; i++) {
             dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
         }
-        
         return dp[len-1];
     }
 };
