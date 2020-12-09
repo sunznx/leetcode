e586ca618a8194297009ec49614da9c000fa67d0
sunznx <sunznx@163.com>
2020-12-10 04:16:47 +0800

    update 70.climbing-stairs.cpp

--- 70.climbing-stairs.cpp
+++ 70.climbing-stairs.cpp
@@ -1,14 +1,13 @@
+// CreateTime: 2020-12-10 04:13:04
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1, 0);
-        
+        vector<int> dp(n+1);
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             dp[i] = dp[i-1] + dp[i-2];
         }
-
         return dp[n];
     }
 };
