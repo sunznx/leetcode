01e99c51e5b7822c69f419b8483052bd02395e45
sunznx <sunznx@163.com>
2020-12-11 18:10:23 +0800

    update 322.coin-change.cpp

--- 322.coin-change.cpp
+++ 322.coin-change.cpp
@@ -5,12 +5,10 @@ public:
         vector<int> dp(amount+1, amount+1);
 
         dp[0] = 0;
-        for (int j = 0; j <= amount; j++) {
-            for (int i = 0; i < coins.size(); i++) {
-                auto x = coins[i];
-                if (j >= x) {
-                    dp[j] = min(dp[j], dp[j-x] + 1);
-                }
+        for (int i = 0; i < coins.size(); i++) {
+            auto x = coins[i];
+            for (int j = x; j <= amount; j++) {
+                dp[j] = min(dp[j], dp[j-x] + 1);
             }
         }
 
