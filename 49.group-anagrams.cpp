0d850f4e1aee32f35ecae505ad55122f6107bb4c
sunznx <sunznx@163.com>
2020-12-14 11:37:05 +0800

    update 49.group-anagrams.cpp

--- 49.group-anagrams.cpp
+++ 49.group-anagrams.cpp
@@ -1,57 +1,42 @@
-// CreateTime: 2019-11-22 00:30:25
+// CreateTime: 2020-12-14 11:36:33
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int len = strs.size();
 
-        vector<unsigned int> p(27);
-        init(p);
+        auto p = init();
 
         unordered_map<unsigned int, vector<string>> m;
 
         for (int i = 0; i < len; i++) {
             auto h = calcHash(p, strs[i]);
-
-            auto item = m.find(h);
-            if (item == m.end()) {
-                vector<string> v;
-                v.push_back(strs[i]);
-                m.insert(make_pair(h, v));
-            } else {
-                (item->second).push_back(strs[i]);
-            }
+            m[h].push_back(strs[i]);
         }
 
         vector<vector<string>> res;
-
         for (auto iter = m.begin(); iter != m.end(); iter++) {
-            int len = iter->second.size();
-
-            vector<string> v;
-            for (int i = 0; i < len; i++) {
-                v.push_back((iter->second)[i]);
-            }
-            res.push_back(v);
+            auto x = iter->second;
+            res.push_back(x);
         }
         return res;
     }
 
-    void init(vector<unsigned int> &p) {
+    vector<unsigned int> init() {
+        vector<unsigned int> p(26);
         int P = 131;
         p[0] = 1;
         for (int i = 1; i < p.size(); i++) {
             p[i] = p[i-1] * P;
         }
+        return p;
     }
 
     unsigned int calcHash(vector<unsigned int> &p, string &s) {
         unsigned int ans = 0;
-
-        int len = s.size();
-        for (int i = 0; i < len; i++) {
-            ans += p[s[i]-'a'];
+        for (int i = 0; i < s.size(); i++) {
+            auto x = s[i]-'a';
+            ans += p[x];
         }
-
         return ans;
     }
 };
