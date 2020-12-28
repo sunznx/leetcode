eba46828ad315f56a7d5e5273a19ef4a7c518b9d
sunznx <sunznx@163.com>
2020-12-28 23:54:38 +0800

    update 108.convert-sorted-array-to-binary-search-tree.1.cpp

---
+++ 108.convert-sorted-array-to-binary-search-tree.1.cpp
@@ -0,0 +1,29 @@
+// CreateTime: 2020-12-28 23:50:21
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return sortedArrayToBST(nums, 0, nums.size()-1);
+    }
+    
+    TreeNode* sortedArrayToBST(vector<int>& nums, int l, int r) {
+        if (l > r) {
+            return NULL;
+        }
+        
+        int m = (l+r) / 2;
+        auto x = nums[m];
+        
+        auto left = sortedArrayToBST(nums, l, m-1);
+        auto right = sortedArrayToBST(nums, m+1, r);
+        return new TreeNode(x, left, right);
+    }
+};
