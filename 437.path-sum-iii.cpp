4c1b47beb24826b45a6d40f225c20b4fa6f62609
sunznx <sunznx@163.com>
2021-01-04 17:27:29 +0800

    update 437.path-sum-iii.cpp

--- 437.path-sum-iii.cpp
+++ 437.path-sum-iii.cpp
@@ -1,34 +1,36 @@
-// CreateTime: 2019-12-02 12:46:03
+// CreateTime: 2021-01-04 17:13:51
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int res = 0;
+    int ans = 0;
 
     int pathSum(TreeNode* root, int sum) {
         unordered_map<int, int> m;
         m[0] = 1;
-        traversal(root, m, sum, 0);
-        return res;
+        helper(root, sum, m);
+        return ans;
     }
 
-    void traversal(TreeNode* root, unordered_map<int, int> m, int sum, int tot) {
+    void helper(TreeNode* root, int sum, unordered_map<int, int> m, int cur = 0) {
         if (root == NULL) {
             return;
         }
 
-        tot += root->val;
-        res += m[tot-sum];
-        m[tot] += 1;
+        cur += root->val;
+        ans += m[cur-sum];
+        m[cur]++;
 
-        traversal(root->left, m, sum, tot);
-        traversal(root->right, m, sum, tot);
+        helper(root->left, sum, m, cur);
+        helper(root->right, sum, m, cur);
     }
 };
