63b4ee5ff59e22934ac4e889f63ee2399b94f4b7
sunznx <sunznx@163.com>
2021-01-04 17:07:51 +0800

    update 124.binary-tree-maximum-path-sum.cpp

--- 124.binary-tree-maximum-path-sum.cpp
+++ 124.binary-tree-maximum-path-sum.cpp
@@ -1,41 +1,44 @@
-// CreateTime: 2019-12-11 17:03:20
+// CreateTime: 2021-01-04 16:17:16
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int ans;
+    int ans = INT_MIN;
 
-    int maxPathSum(TreeNode* root) {
-        ans = root->val;
-        maxPathSumHelper(root);
+    int maxPathSum(TreeNode *root) {
+        helper(root);
         return ans;
     }
 
-    int maxPathSumHelper(TreeNode *root) {
+    int helper(TreeNode *root) {
         auto l = 0;
         auto r = 0;
+
         if (root->left) {
-            l = maxPathSumHelper(root->left);
+            l = helper(root->left);
         }
+
         if (root->right) {
-            r = maxPathSumHelper(root->right);
+            r = helper(root->right);
         }
 
         ans = max(ans, root->val);
-        ans = max(ans, root->val + l);
-        ans = max(ans, root->val + r);
-        ans = max(ans, root->val + l + r);
+        ans = max(ans, root->val+l);
+        ans = max(ans, root->val+r);
+        ans = max(ans, root->val+l+r);
 
         int ret = root->val;
-        ret = max(ret, root->val + l);
-        ret = max(ret, root->val + r);
+        ret = max(ret, l+root->val);
+        ret = max(ret, r+root->val);
         return ret;
     }
 };
