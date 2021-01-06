c2592e11a33c102075e52dc36992c65208f21b16
sunznx <sunznx@163.com>
2021-01-07 01:59:00 +0800

    update 930.binary-subarrays-with-sum.cpp

--- 930.binary-subarrays-with-sum.cpp
+++ 930.binary-subarrays-with-sum.cpp
@@ -4,19 +4,14 @@ public:
     int numSubarraysWithSum(vector<int>& A, int S) {
         int ans = 0;
 
+        unordered_map<int, int> m;
+        m[0] = 1;
+
         int sum = 0;
-        unordered_map<int, int> p;
-        p[0] = 1;
-
         for (int i = 0; i < A.size(); i++) {
-            auto x = A[i];
-            sum += x;
-
-            if (sum >= S) {
-                ans += p[sum - S];
-            }
-
-            p[sum]++;
+            sum += A[i];
+            ans += m[sum-S];
+            m[sum]++;
         }
 
         return ans;
