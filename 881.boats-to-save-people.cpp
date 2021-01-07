e0dae833dc0ea9fd21e1fa8dfdf84a0a0c89b6cc
sunznx <sunznx@163.com>
2021-01-07 08:56:03 +0800

    update 881.boats-to-save-people.cpp

--- 881.boats-to-save-people.cpp
+++ 881.boats-to-save-people.cpp
@@ -1,30 +1,21 @@
-// CreateTime: 2020-11-24 20:33:36
+// CreateTime: 2021-01-07 08:45:42
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        int len = people.size();
-
-        sort(people.begin(), people.end());
-
-        int l = 0;
-        int r = len-1;
-
         int ans = 0;
 
+        int l = 0;
+        int r = people.size()-1;
+        sort(people.begin(), people.end());
+
         while (l <= r) {
-            if (l == r) {
+            auto reset = limit-people[r--];
+            ans++;
+
+            if (reset >= people[l]) {
                 l++;
-                ans++;
-            } else if (people[l]+people[r] <= limit) {
-                l++;
-                r--;
-                ans++;
-            } else if (people[r] <= limit) {
-                r--;
-                ans++;
             }
         }
-
         return ans;
     }
 };
