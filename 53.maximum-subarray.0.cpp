0f3fea01a3da4b56c2482fee3cab3fb376928733
sunznx <sunznx@163.com>
2021-01-09 16:33:41 +0800

    update 53.maximum-subarray.0.cpp

---
+++ 53.maximum-subarray.0.cpp
@@ -0,0 +1,22 @@
+// CreateTime: 2021-01-09 16:30:14
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+
+        int ans = nums[0];
+        int sub = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            auto x = nums[i];
+            sub += x;
+
+            ans = max({ans, sub, x});
+            sub = max({sub, x});
+        }
+
+        return ans;
+    }
+};
