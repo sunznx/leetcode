f1550b2fd3f0d437af0aad00a5a1b56aba76cab2
sunznx <sunznx@163.com>
2021-01-17 06:17:06 +0000

    update 5243.tuple-with-same-product.cpp

---
+++ 5243.tuple-with-same-product.cpp
@@ -0,0 +1,28 @@
+// CreateTime: 2021-01-17 10:34:21
+class Solution {
+public:
+    typedef pair<int, int> PII;
+    unordered_map<int, vector<PII>> m;
+
+    int ans = 0;
+
+    int tupleSameProduct(vector<int>& nums) {
+        // sort(nums.begin(), nums.end());
+        unique(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i+1; j < nums.size(); j++) {
+                auto x = nums[i];
+                auto y = nums[j];
+                m[x*y].push_back({i, j});
+            }
+        }
+
+        for (auto &x: m) {
+            auto k = x.second.size();
+            ans += k * (k-1) / 2 * 8;
+        }
+
+        return ans;
+    }
+};
