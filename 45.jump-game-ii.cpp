f39a87ac3046a1ac7fa74f1b718a94c9e9f4b530
sunznx <sunznx@163.com>
2021-01-17 19:45:03 +0000

    update 45.jump-game-ii.cpp

--- 45.jump-game-ii.cpp
+++ 45.jump-game-ii.cpp
@@ -2,20 +2,20 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int W = nums.size()-1;
+        int len = nums.size();
 
-        vector<int> f(W+1, W+1);
+        vector<int> f(len, len);
 
         f[0] = 0;
         int w = 0;
         for (int i = 0; i < nums.size()-1; i++) {
             auto x = nums[i];
-            for (int j = w; j <= i+x && j <= W; j++) {
+            for (int j = w+1; j <= i+x && j <= len-1; j++) {
                 f[j] = min(f[j], f[i] + 1);
             }
             w = x + i;
         }
-        return f[W];
+        return f[len-1];
     }
 };
 
