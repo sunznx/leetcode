a7ac69358af76512b72356432005cd53f13726b3
sunznx <sunznx@163.com>
2021-01-18 16:21:06 +0000

    update 303.range-sum-query-immutable.cpp

--- 303.range-sum-query-immutable.cpp
+++ 303.range-sum-query-immutable.cpp
@@ -1,22 +1,17 @@
+// CreateTime: 2021-01-19 00:19:27
 class NumArray {
 public:
-    vector<int> sum;
+    vector<int> p;
 
     NumArray(vector<int> nums) {
-        int len = nums.size();
+        p.resize(nums.size()+1);
 
-        int total = 0;
-        for (int i = 0; i < len; i++) {
-            total += nums[i];
-            sum.push_back(total);
+        for (int i = 0; i < nums.size(); i++) {
+            p[i+1] = p[i] + nums[i];
         }
     }
 
     int sumRange(int i, int j) {
-        if (i == 0) {
-            return sum[j];
-        }
-
-        return sum[j] - sum[i-1];
+        return p[j+1] - p[i];
     }
 };
