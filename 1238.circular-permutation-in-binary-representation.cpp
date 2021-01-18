29d3ff1c373bcca2f9e4d7c1b77a84fe62c30ca3
sunznx <sunznx@163.com>
2021-01-18 18:00:31 +0000

    update 1238.circular-permutation-in-binary-representation.cpp

---
+++ 1238.circular-permutation-in-binary-representation.cpp
@@ -0,0 +1,22 @@
+// CreateTime: 2021-01-18 17:49:51
+class Solution {
+public:
+    int p = 0;
+    vector<int> ans;
+
+    vector<int> circularPermutation(int n, int start) {
+        for (int i = 0; i < (1<<n); i++) {
+            auto x = (i^(i>>1));
+            ans.push_back(x);
+
+            if (x == start) {
+                p = i;
+            }
+        }
+
+        reverse(ans.begin(), ans.begin()+p);
+        reverse(ans.begin()+p, ans.end());
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
