6770f22d27969fc7cbf646027304ddc76d8e2763
sunznx <sunznx@163.com>
2021-01-23 13:32:15 +0000

    update 1655.distribute-repeating-integers.cpp

---
+++ 1655.distribute-repeating-integers.cpp
@@ -0,0 +1,63 @@
+// CreateTime: 2021-01-23 12:49:47
+class Solution {
+public:
+
+    unordered_map<int, int> m;
+    vector<int> counts;
+
+    bool canDistribute(vector<int>& nums, vector<int>& quantity) {
+        for (auto &x: nums) {
+            m[x]++;
+        }
+
+        for (auto &x: m) {
+            counts.push_back(x.second);
+        }
+
+        int m = quantity.size();
+        vector<int> sum(1<<m);
+        for (int i = 0; i < (1<<m); i++) {
+            for (int j = 0; j < m; j++) {
+                if ((i & (1<<j)) != 0) {
+                    auto x = (i-(1<<j));
+                    sum[i] = sum[x] + quantity[j];
+                }
+            }
+        }
+
+        vector<vector<bool>> f(counts.size(), vector<bool>((1<<m)));
+        for (int i = 0; i < counts.size(); i++) {
+            f[i][0] = true;
+        }
+
+        for (int i = 0; i < counts.size(); i++) {
+            for (int j = 0; j < (1<<m); j++) {
+                if (i > 0 && f[i-1][j] == true) {
+                    f[i][j] = true;
+                    continue;
+                }
+
+                for (int s = j; s > 0; s = ((s-1)&j)) {
+                    auto prev = j-s;
+                    bool prevOk = false;
+                    if (i == 0) {
+                        prevOk = (prev == 0);
+                    } else {
+                        prevOk = f[i-1][prev];
+                    }
+
+                    if (!prevOk) {
+                        continue;
+                    }
+
+                    if (sum[s] <= counts[i]) {
+                        f[i][j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return f[counts.size()-1][(1<<m)-1];
+    }
+};
