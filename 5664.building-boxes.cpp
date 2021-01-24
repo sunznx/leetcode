2a5e985e6370a543fec1643e540e5945fe72514a
sunznx <sunznx@163.com>
2021-01-24 07:49:54 +0000

    update 5664.building-boxes.cpp

--- 5664.building-boxes.cpp
+++ 5664.building-boxes.cpp
@@ -1,23 +1,35 @@
 // CreateTime: 2021-01-24 11:23:11
 class Solution {
 public:
+
+    /*
+      1
+      1 + 2
+      1 + 2 + 3
+      ...
+      1 + 2 + .. + k
+      
+      剩余部分报团取暖
+      1 + 2 + .. + k = 1/2 * (k * (k + 1))
+    */
+
     int minimumBoxes(int n) {
         int sum = 0;
         int k = 1;
-        
+
         while (sum + (k*(k+1))/2 <= n) {
             sum += k*(k+1)/2;
             k++;
         }
-        
+
         int bottom = (k-1)*k/2;
-        
+
         int newBottom = 1;
         while (sum < n) {
             sum += newBottom;
             newBottom++;
             bottom++;
-        }    
+        }
         return bottom;
     }
 };
