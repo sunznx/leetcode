8650b5842b8025b0b6c68c6b2de2e0681f6536f6
sunznx <sunznx@163.com>
2021-02-08 16:28:47 +0800

    update 1353.maximum-number-of-events-that-can-be-attended.cpp

--- 1353.maximum-number-of-events-that-can-be-attended.cpp
+++ 1353.maximum-number-of-events-that-can-be-attended.cpp
@@ -2,22 +2,27 @@
 class Solution {
 public:
     int ans = 0;
-    const int MAX = 1e5;
     priority_queue<int, vector<int>, greater<int>> pq;
 
     int maxEvents(vector<vector<int>>& events) {
         unordered_map<int, vector<int>> m;
+
+        int l = INT_MAX;
+        int r = INT_MIN;
 
         for (auto &x: events) {
             auto start = x[0];
             auto end = x[1];
 
             m[start].push_back(end);
+
+            l = min(l, start);
+            r = max(r, end);
         }
 
-        for (int i = 1; i <= MAX; i++) {
-            for (auto &end: m[i]) {
-                pq.push(end);
+        for (int i = l; i <= r; i++) {
+            for (auto &x: m[i]) {
+                pq.push(x);
             }
 
             // 过滤掉过期的时间
