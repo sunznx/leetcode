b71b61d55552bcb737b56809a1faf04051336147
sunznx <sunznx@163.com>
2021-02-18 15:24:12 +0800

    update 1752.check-if-array-is-sorted-and-rotated.cpp

---
+++ 1752.check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,22 @@
+// CreateTime: 2021-02-07 02:44:07
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int len = nums.size();
+        int maxn = 0;
+        int sub = 1;
+        for (int i = 1; i < len*2; i++) {
+            auto x = nums[i%len];
+            auto pre = nums[(i-1)%len];
+            
+            if (x >= pre) {
+                sub++;
+            } else {
+                sub = 1;
+            }
+            maxn = max(maxn, sub);
+        }
+        
+        return maxn >= len;
+    }
+};
