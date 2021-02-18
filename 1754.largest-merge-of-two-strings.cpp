c99878f6b20776031301f4ea44f25d9db037344e
sunznx <sunznx@163.com>
2021-02-18 16:51:54 +0800

    update 1754.largest-merge-of-two-strings.cpp

--- 1754.largest-merge-of-two-strings.cpp
+++ 1754.largest-merge-of-two-strings.cpp
@@ -4,19 +4,40 @@ public:
     string largestMerge(string word1, string word2) {
         string ans = "";
 
-        while (word1.size() || word2.size()) {
-            if (word1.size() == 0) {
-                ans = ans + word2;
-                word2.clear();
-            } else if (word2.size() == 0) {
-                ans = ans + word1;
-                word1.clear();
-            } else if (word1 > word2) {
-                ans = ans + word1[0];
-                word1 = word1.substr(1);
+        int len1 = word1.size();
+        int len2 = word2.size();
+
+        auto i = 0;
+        auto j = 0;
+        auto same = 0;
+        while (ans.size() < len1+len2) {
+            if (i == len1) {
+                ans = ans + word2.substr(j);
+                continue;
+            }
+
+            if (j == len2) {
+                ans = ans + word1.substr(i);
+                continue;
+            }
+
+            auto I = i+same;
+            auto J = j+same;
+
+            if (I == len1) {
+                ans += word2[j++];
+                same = 0;
+            } else if (J == len2) {
+                ans += word1[i++];
+                same = 0;
+            } else if (word1[I] > word2[J]) {
+                ans += word1[i++];
+                same = 0;
+            } else if (word1[I] < word2[J]) {
+                ans += word2[j++];
+                same = 0;
             } else {
-                ans = ans + word2[0];
-                word2 = word2.substr(1);
+                same++;
             }
         }
 
