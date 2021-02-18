d41422747e9ef6c0ec986c734b39cf2b36820ff0
sunznx <sunznx@163.com>
2021-02-18 17:02:35 +0800

    update 1754.largest-merge-of-two-strings.cpp

--- 1754.largest-merge-of-two-strings.cpp
+++ 1754.largest-merge-of-two-strings.cpp
@@ -9,21 +9,11 @@ public:
 
         auto i = 0;
         auto j = 0;
-        while (true) {
+        while (i < word1.size() || j < word2.size()) {
             if (word1.substr(i) > word2.substr(j)) {
                 ans += word1[i++];
             } else {
                 ans += word2[j++];
-            }
-
-            if (i == len1) {
-                ans = ans + word2.substr(j);
-                break;
-            }
-
-            if (j == len2) {
-                ans = ans + word1.substr(i);
-                break;
             }
         }
 
