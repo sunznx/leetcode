f2c7db7b01ad2d362ed16eb026b5d25712fd49dc
sunznx <sunznx@163.com>
2021-02-19 14:14:31 +0800

    update 121.best-time-to-buy-and-sell-stock.cpp

--- 121.best-time-to-buy-and-sell-stock.cpp
+++ 121.best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,13 @@
-// CreateTime: 2019-12-15 10:03:40
+// CreateTime: 2021-02-19 14:09:57
 class Solution {
 public:
-
     int maxProfit(vector<int>& prices) {
         int ans = 0;
-        int minv = 0x7fffffff;
 
-        for (int i = 0; i < prices.size(); i++) {
-            auto x = prices[i];
-            ans = max(ans, x - minv);
-            minv = min(minv, x);
+        int minVal = prices[0];
+        for (auto &x: prices) {
+            ans = max(ans, x-minVal);
+            minVal = min(minVal, x);
         }
 
         return ans;
