8efe455d0244bda5ac9570c2656c92ef0f8c78b0
sunznx <sunznx@163.com>
2021-02-19 10:52:42 +0800

    update 1011.capacity-to-ship-packages-within-d-days.cpp

--- 1011.capacity-to-ship-packages-within-d-days.cpp
+++ 1011.capacity-to-ship-packages-within-d-days.cpp
@@ -1,15 +1,9 @@
-// CreateTime: 2020-11-25 13:49:19
+// CreateTime: 2021-02-19 10:44:37
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int D) {
-        int l = 0;
-        int r = 0;
-        
-        int len = weights.size();
-        for (int i = 0; i < len; i++) {
-            r = r + weights[i];
-            l = max(l, weights[i]);
-        }
+        int l = *max_element(weights.begin(), weights.end());
+        int r = accumulate(weights.begin(), weights.end(), 0);
 
         while (l < r) {
             int m = (l+r) / 2;
@@ -19,21 +13,23 @@ public:
                 l = m+1;
             }
         }
+
         return l;
     }
-    
-    bool check(vector<int>& weights, int D, int w) {
-        int need = 0;
-        int len = weights.size();
-        
-        int reserve = 0;
-        for (int i = 0; i < len; i++) {
-            if (reserve < weights[i]) {
-                reserve = w;
-                need++;
+
+    inline bool check(vector<int>& weights, int D, int k) {
+        int ans = 0;
+
+        auto leaves = 0;
+        for (auto &x: weights) {
+            if (x > leaves) {
+                leaves = k-x;
+                ans++;
+            } else {
+                leaves = leaves-x;
             }
-            reserve -= weights[i];
         }
-        return need <= D;
+
+        return ans <= D;
     }
 };
