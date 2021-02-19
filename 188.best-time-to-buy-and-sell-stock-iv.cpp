fc39507f4eceb02df81dd97fe95889d6bfb8d665
sunznx <sunznx@163.com>
2021-02-19 17:00:46 +0800

    update 188.best-time-to-buy-and-sell-stock-iv.cpp

--- 188.best-time-to-buy-and-sell-stock-iv.cpp
+++ 188.best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,4 +1,4 @@
-// CreateTime: 2021-01-09 17:45:02
+// CreateTime: 2021-02-19 16:55:54
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
@@ -6,29 +6,30 @@ public:
             return 0;
         }
 
-        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(k, vector<int>(2, 0)));
+        int len = prices.size();
+        vector<vector<vector<int>>> f(len, vector<vector<int>>(k, vector<int>(2)));
 
-        // dp[i][j][0]  表示在 i 状态下，持有 j 股票
-        // dp[i][j][0]  表示在 i 状态下，卖出 j 股票
+        // f[i][k][0] 表示持有股票 k
+        // f[i][k][1] 表示没持有股票 k
 
-        for (int i = 0; i < k; i++) {
-            dp[0][i][0] = -prices[0];
+        for (int j = 0; j < k; j++) {
+            f[0][j][0] = -prices[0];
         }
-
-        for (int i = 1; i < prices.size(); i++) {
+        for (int i = 1; i < len; i++) {
             auto x = prices[i];
 
-            dp[i][0][0] = max(dp[i-1][0][0], -x);
-            dp[i][0][1] = max(dp[i-1][0][1], dp[i-1][0][0]+x);
+            f[i][0][0] = max(f[i-1][0][0], -x);
+            f[i][0][1] = max(f[i-1][0][1], f[i-1][0][0]+x);
+
             for (int j = 1; j < k; j++) {
-                dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j-1][1]-x);
-                dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j][0]+x);
+                f[i][j][0] = max(f[i-1][j][0], f[i-1][j-1][1]-x);
+                f[i][j][1] = max(f[i-1][j][1], f[i-1][j][0]+x);
             }
         }
 
-        int ans = 0;
+        auto ans = 0;
         for (int j = 0; j < k; j++) {
-            ans = max(ans, dp[prices.size()-1][j][1]);
+            ans = max({ans, f[len-1][j][0], f[len-1][j][1]});
         }
         return ans;
     }
