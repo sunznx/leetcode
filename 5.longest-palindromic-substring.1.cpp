2fabd5bd80a6de15252520719396251d4e7f07e8
sunznx <sunznx@163.com>
2021-03-01 19:34:01 +0800

    update 5.longest-palindromic-substring.1.cpp

---
+++ 5.longest-palindromic-substring.1.cpp
@@ -0,0 +1,30 @@
+// CreateTime: 2021-03-01 19:28:31
+class Solution {
+public:
+    string ans = "";
+
+    string longestPalindrome(string s) {
+        int len = s.size();
+        for (int i = 0; i < len; i++) {
+            tryExpand(s, i, i);
+            tryExpand(s, i, i+1);
+        }
+        return ans;
+    }
+
+    void tryExpand(string &s, int k1, int k2) {
+        int len = s.size();
+        while (k1 >= 0 && k2 < len) {
+            if (s[k1] == s[k2]) {
+                if (k2-k1+1 > ans.size()) {
+                    ans = s.substr(k1, k2-k1+1);
+                }
+
+                k1--;
+                k2++;
+            } else {
+                break;
+            }
+        }
+    }
+};
