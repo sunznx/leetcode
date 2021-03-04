4191229f88a9b7122469e9b172b9606e4c2b42a1
sunznx <sunznx@163.com>
2021-03-04 10:45:14 +0800

    update 125.valid-palindrome.0.cpp

---
+++ 125.valid-palindrome.0.cpp
@@ -0,0 +1,63 @@
+// CreateTime: 2021-03-04 10:15:43
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int r = s.size()-1;
+
+        while (l < r) {
+            while (l < r && !isOk(s[l])) {
+                l++;
+            }
+
+            while (l < r && !isOk(s[r])) {
+                r--;
+            }
+
+            if (l < r && !isEqual(s[l], s[r])) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    bool isEqual(char c1, char c2) {
+        int s1 = 0;
+        int s2 = 0;
+        if (isNum(c1)) {
+            s1 = c1 - '0';
+        } else if (isUpper(c1)) {
+            s1 = c1 - 'A' + 10;
+        } else {
+            s1 = c1 - 'a' + 10;
+        }
+
+        if (isNum(c2)) {
+            s2 = c2 - '0';
+        } else if (isUpper(c2)) {
+            s2 = c2 - 'A' + 10;
+        } else {
+            s2 = c2 - 'a' + 10;
+        }
+        return s1 == s2;
+    }
+
+    bool isOk(char c) {
+        return isNum(c) || isLower(c) || isUpper(c);
+    }
+
+    bool isNum(char c) {
+        return '0' <= c && c <= '9';
+    }
+
+    bool isLower(char c) {
+        return 'a' <= c && c <= 'z';
+    }
+
+    bool isUpper(char c) {
+        return 'A' <= c && c <= 'Z';
+    }
+};
