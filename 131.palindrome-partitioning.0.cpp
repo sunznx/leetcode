33189abf926c168f223dd8691104661d3a84aca1
sunznx <sunznx@163.com>
2021-03-08 17:56:57 +0800

    update 131.palindrome-partitioning.0.cpp

--- 131.palindrome-partitioning.0.cpp
+++ 131.palindrome-partitioning.0.cpp
@@ -1,25 +1,30 @@
 // CreateTime: 2021-03-08 16:29:18
 class Solution {
 public:
+    typedef pair<bool, string> PBS;
     vector<vector<string>> ans;
-    vector<vector<bool>> dp;
+    vector<vector<PBS>> dp;
     vector<string> sub;
 
     vector<vector<string>> partition(string s) {
         int len = s.size();
-        dp.resize(len+1, vector<bool>(len+1));
+        dp.resize(len+1, vector<PBS>(len+1));
 
         for (int i = 0; i < len; i++) {
-            dp[i][i] = true;
+            dp[i][i].first = true;
+            dp[i][i].second.push_back(s[i]);
+
             if (i > 0 && s[i] == s[i-1]) {
-                dp[i-1][i] = true;
+                dp[i-1][i].first = true;
+                dp[i-1][i].second = string(1, s[i-1]) + string(1, s[i]);
             }
         }
 
         for (int i = len-1; i >= 0; i--) {
             for (int j = i+2; j < len; j++) {
-                if ((dp[i+1][j-1]) && s[i] == s[j]) {
-                    dp[i][j] = true;
+                if ((dp[i+1][j-1].first) && s[i] == s[j]) {
+                    dp[i][j].first = true;
+                    dp[i][j].second = string(1, s[i]) + dp[i+1][j-1].second + string(1, s[j]);
                 }
             }
         }
@@ -35,8 +40,8 @@ public:
         }
 
         for (int k = step; k <= len-1; k++) {
-            if (dp[step][k]) {
-                sub.push_back(s.substr(step, k-step+1));
+            if (dp[step][k].first) {
+                sub.push_back(dp[step][k].second);
                 dfs(s, len, k+1);
                 sub.pop_back();
             }
