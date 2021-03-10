272237cc3ca3106bbc06ed64a80d1dc01e296800
sunznx <sunznx@163.com>
2021-03-10 21:03:11 +0800

    update 516.longest-palindromic-subsequence.0.cpp

---
+++ 516.longest-palindromic-subsequence.0.cpp
@@ -0,0 +1,28 @@
+// CreateTime: 2021-03-10 21:00:18
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int len = s.size();
+
+        vector<vector<int>> f(len+1, vector<int>(len+1));
+
+        for (int i = 0; i < len; i++) {
+            f[i][i] = 1;
+            if (i > 0 && s[i] == s[i-1]) {
+                f[i-1][i] = 2;
+            }
+        }
+
+        for (int i = len-1; i >= 0; i--) {
+            for (int j = i+1; j < len; j++) {
+                if (s[i] == s[j]) {
+                    f[i][j] = max(f[i][j], f[i+1][j-1]+2);
+                } else {
+                    f[i][j] = max({f[i][j], f[i][j-1], f[i+1][j]});
+                }
+            }
+        }
+
+        return f[0][len-1];
+    }
+};
