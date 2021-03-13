9d08dd3910604306c963f025ab574eed708e5202
sunznx <sunznx@163.com>
2021-03-14 06:54:41 +0800

    update 131.palindrome-partitioning.0.cpp

--- 131.palindrome-partitioning.0.cpp
+++ 131.palindrome-partitioning.0.cpp
@@ -7,19 +7,11 @@ public:
 
     vector<vector<string>> partition(string s) {
         int len = s.size();
-        dp.resize(len+1, vector<bool>(len+1));
-
-        for (int i = 0; i < len; i++) {
-            dp[i][i] = true;
-            if (i > 0 && s[i] == s[i-1]) {
-                dp[i-1][i] = true;
-            }
-        }
-
+        dp.resize(len+1, vector<bool>(len+1, true));
         for (int i = len-1; i >= 0; i--) {
-            for (int j = i+2; j < len; j++) {
-                if ((dp[i+1][j-1]) && s[i] == s[j]) {
-                    dp[i][j] = true;
+            for (int j = i+1; j < len; j++) {
+                if (!(s[i] == s[j] && dp[i+1][j-1])) {
+                    dp[i][j] = false;
                 }
             }
         }
