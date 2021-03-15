6c93f5aebb2ad531bac4ab5e1921848ad3b36d6d
sunznx <sunznx@163.com>
2021-03-15 20:10:31 +0800

    update 647.palindromic-substrings.0.cpp

--- 647.palindromic-substrings.0.cpp
+++ 647.palindromic-substrings.0.cpp
@@ -1,23 +1,25 @@
-// CreateTime: 2021-03-15 20:03:50
+// CreateTime: 2021-03-15 20:07:50
 class Solution {
 public:
+    int ans = 0;
     int countSubstrings(string s) {
         int len = s.size();
-        vector<vector<bool>> f(len+1, vector<bool>(len+1, true));
-
-        int ans = 0;
-        for (int i = len-1; i >= 0; i--) {
-            ans++;
-            for (int j = i+1; j < len; j++) {
-                if (!(s[i] == s[j] && f[i+1][j-1])) {
-                    f[i][j] = false;
-                }
-
-                if (f[i][j]) {
-                    ans++;
-                }
-            }
+        for (int i = 0; i < len; i++) {
+            tryExpand(s, i, i);
+            tryExpand(s, i, i+1);
         }
+
         return ans;
+    }
+
+    void tryExpand(string &s, int k1, int k2) {
+        int l = 0;
+        int r = s.size()-1;
+
+        while (l <= k1 && k1 <= r && l <= k2 && k2 <= r && s[k1] == s[k2]) {
+            k1--;
+            k2++;
+            ans++;
+        }
     }
 };
