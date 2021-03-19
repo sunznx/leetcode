a213596cbf8d5859cc25c6630a18a98d1cae327b
sunznx <sunznx@163.com>
2021-03-19 08:18:02 +0800

    update 206.reverse-linked-list.0.cpp

---
+++ 206.reverse-linked-list.0.cpp
@@ -0,0 +1,32 @@
+// CreateTime: 2021-03-19 08:13:26
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *p = head;
+        ListNode *newList = NULL;
+
+        while (p) {
+            auto next = p->next;
+            if (newList == NULL) {
+                newList = p;
+                newList->next = NULL;
+            } else {
+                p->next = newList;
+                newList = p;
+            }
+            p = next;
+        }
+
+        return newList;
+    }
+};
