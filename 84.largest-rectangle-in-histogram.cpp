ef225e73ec3b814f164b5dc15e4132cc093a8d06
sunznx <sunznx@163.com>
2021-03-31 16:10:39 +0800

    update 84.largest-rectangle-in-histogram.cpp

--- 84.largest-rectangle-in-histogram.cpp
+++ 84.largest-rectangle-in-histogram.cpp
@@ -1,47 +1,50 @@
-// CreateTime: 2019-11-21 22:49:31
+// CreateTime: 2021-03-31 15:49:47
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        int ans = 0;
         int len = heights.size();
+        stack<int> stk;
+        vector<int> l(len);
+        vector<int> r(len);
 
-        vector<int> l(len);   // 左边比他大的数有多少个
-        vector<int> r(len);   // 右边比他大的数有多少个
-
+        stk = stack<int>();
         for (int i = 0; i < len; i++) {
-            int k = 0;
-            for (int j = i-1; j >= 0; j--) {
-                if (heights[j] == heights[i]) {
-                    k += l[j] + 1;
-                    break;
-                } else if (heights[j] > heights[i]) {
-                    k++;
-                } else {
-                    break;
-                }
+            auto x = heights[i];
+
+            while (stk.size() && x <= heights[stk.top()]) {
+                stk.pop();
             }
-            l[i] = k;
+
+            if (stk.size() == 0) {
+                l[i] = i;
+            } else {
+                l[i] = i-stk.top()-1;
+            }
+
+            stk.push(i);
         }
 
+        stk = stack<int>();
         for (int i = len-1; i >= 0; i--) {
-            int k = 0;
-            for (int j = i+1; j < len; j++) {
-                if (heights[j] == heights[i]) {
-                    k += r[j] + 1;
-                    break;
-                } else if (heights[j] > heights[i]) {
-                    k++;
-                } else {
-                    break;
-                }
+            auto x = heights[i];
+
+            while (stk.size() && x <= heights[stk.top()]) {
+                stk.pop();
             }
-            r[i] = k;
+
+            if (stk.size() == 0) {
+                r[i] = len-1-i;
+            } else {
+                r[i] = stk.top()-i-1;
+            }
+
+            stk.push(i);
         }
 
-        int res = 0;
         for (int i = 0; i < len; i++) {
-            res = max(res, (l[i]+1+r[i]) * heights[i]);
+            ans = max(ans, heights[i] * (l[i] + 1 + r[i]));
         }
-
-        return res;
+        return ans;
     }
 };
