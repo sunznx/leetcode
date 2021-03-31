7afe7d69f32032a5e458f74a10c9da82095c21aa
sunznx <sunznx@163.com>
2021-03-31 15:34:12 +0800

    update 239.sliding-window-maximum.cpp

--- 239.sliding-window-maximum.cpp
+++ 239.sliding-window-maximum.cpp
@@ -1,28 +1,29 @@
-// CreateTime: 2019-11-21 21:22:37
+// CreateTime: 2021-03-31 15:28:07
 class Solution {
 public:
+    vector<int> ans;
+
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int len = nums.size();
+        deque<int> que;
 
-        deque<pair<int, int>> q;
-        vector<int> res;
+        for (int i = 0; i < nums.size(); i++) {
+            auto x = nums[i];
 
-        for (int i = 0; i < len; i++) {
-            while (!q.empty() && nums[i] > q.back().first) {
-                q.pop_back();
+            while (que.size() && que.front()+k-1 < i) {
+                que.pop_front();
             }
 
-            while (!q.empty() && q.front().second+k <= i) {
-                q.pop_front();
+            while (que.size() && x >= nums[que.back()]) {
+                que.pop_back();
             }
 
-            q.push_back({nums[i], i});
+            que.push_back(i);
 
             if (i >= k-1) {
-                res.push_back(q.front().first);
+                ans.push_back(nums[que.front()]);
             }
         }
 
-        return res;
+        return ans;
     }
 };
