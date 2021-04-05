c3ee48a7312d702d2448abe033f4d8428c675713
sunznx <sunznx@163.com>
2021-04-06 00:55:04 +0800

    update 80.remove-duplicates-from-sorted-array-ii.cpp

--- 80.remove-duplicates-from-sorted-array-ii.cpp
+++ 80.remove-duplicates-from-sorted-array-ii.cpp
@@ -1,28 +1,15 @@
+// CreateTime: 2021-04-06 00:50:30
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int len = nums.size();
-        int removed = 0;
-        int samed = 1;
-        
-        for (int i = 1; i < len; i++) {
-            if (nums[i] == nums[i-1]) {
-                samed += 1;
-            }
-            
-            if (nums[i] != nums[i-1] || i == len-1) {
-                int willRemove = samed-2;
-                samed = 1;
-
-                if (willRemove > 0) {
-                    reverse(nums.begin()+removed, nums.begin()+i);
-                    removed += willRemove;
-                    reverse(nums.begin()+removed, nums.begin()+i);
-                }
+        int k = 0;
+        while (k < nums.size()) {
+            if (k > 1 && nums[k] == nums[k-1] &&  nums[k] == nums[k-2]) {
+                nums.erase(nums.begin()+k);
+            } else {
+                k++;
             }
         }
-
-        nums.erase(nums.begin(), nums.begin() + removed);
         return nums.size();
     }
 };
