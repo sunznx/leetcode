17015aa2ca2e8039f3b8ed7283ac256f699a5a94
sunznx <sunznx@163.com>
2021-04-06 01:10:49 +0800

    update 26.remove-duplicates-from-sorted-array.cpp

--- 26.remove-duplicates-from-sorted-array.cpp
+++ 26.remove-duplicates-from-sorted-array.cpp
@@ -2,15 +2,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int k = 0;
-        while (k < nums.size()) {
-            if (k > 0 && nums[k] == nums[k-1]) {
-                nums.erase(nums.begin()+k);
-            } else {
-                k++;
+        if (nums.size() == 0) {
+            return 0;
+        }
+
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[i] != nums[j]) {
+                nums[++i] = nums[j];
             }
         }
-        return nums.size();
+        return i+1;
     }
 };
-
