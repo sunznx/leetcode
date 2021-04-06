af67f381862f8ee0cbb46cee283d3780d2506f2d
sunznx <sunznx@163.com>
2021-04-06 20:24:43 +0800

    update 80.remove-duplicates-from-sorted-array-ii.cpp

--- 80.remove-duplicates-from-sorted-array-ii.cpp
+++ 80.remove-duplicates-from-sorted-array-ii.cpp
@@ -2,13 +2,8 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int len = nums.size();
-        if (len <= 2) {
-            return len;
-        }
-
         int i = 0;
-        for (int j = 1; j < len; j++) {
+        for (int j = 1; j < nums.size(); j++) {
             if (nums[j] != nums[i] || (i == 0 || nums[i] != nums[i-1])) {
                 nums[++i] = nums[j];
             }
