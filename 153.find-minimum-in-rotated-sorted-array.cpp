c135f4265ede786e50f1bc59d69f4056c79c6004
sunznx <sunznx@163.com>
2021-04-08 08:50:02 +0800

    update 153.find-minimum-in-rotated-sorted-array.cpp

--- 153.find-minimum-in-rotated-sorted-array.cpp
+++ 153.find-minimum-in-rotated-sorted-array.cpp
@@ -1,20 +1,21 @@
-// CreateTime: 2019-11-23 09:35:43
+// CreateTime: 2021-04-08 08:44:12
 class Solution {
 public:
     int findMin(vector<int>& nums) {
         int l = 0;
         int r = nums.size()-1;
-        int res = nums[0];
-        while (l < r) {
-            int m = (l+r+1) / 2;
-            if (nums[m] <= nums[0]) {
-                res = nums[m];
-                r = m-1;
-            } else {
-                l = m;
-            }
+        return findMin(nums, l, r);
+    }
+
+    int findMin(vector<int>& nums, int l, int r) {
+        if (l == r) {
+            return nums[l];
         }
 
-        return res;
+        int m = (l+r) / 2;
+        if (nums[m] > nums[r]) {
+            return findMin(nums, m+1, r);
+        }
+        return findMin(nums, l, m);
     }
 };
