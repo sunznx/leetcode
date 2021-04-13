ef2f6abd92242ba31f5b7d6ca0efdfd0716c34c2
sunznx <sunznx@163.com>
2021-04-14 00:06:58 +0800

    update 208.implement-trie-prefix-tree.cpp

--- 208.implement-trie-prefix-tree.cpp
+++ 208.implement-trie-prefix-tree.cpp
@@ -1,20 +1,13 @@
-// CreateTime: 2019-12-23 16:05:22
+// CreateTime: 2021-04-14 00:01:56
 class Trie {
 public:
-
-    struct node {
-        int count;
-        struct node *children[26];
-
-        node () {
-            count = 0;
-            for (int i = 0; i < 26; i++) {
-                children[i] = NULL;
-            }
-        }
+    class node {
+    public:
+        struct node *child[26];
+        bool isEnd = false;
     };
 
-    struct node *root;
+    node *root;
 
     /** Initialize your data structure here. */
     Trie() {
@@ -23,45 +16,48 @@ public:
 
     /** Inserts a word into the trie. */
     void insert(string word) {
-        struct node *tmp = root;
-        for (int i = 0; i < word.size(); i++) {
-            int idx = word[i] - 'a';
-            if (tmp->children[idx] == NULL) {
-                tmp->children[idx] = new node();
+        auto n = root;
+        for (auto &x: word) {
+            auto k = x-'a';
+            if (n->child[k] == NULL) {
+                n->child[k] = new node();
             }
-
-            if (i == word.size()-1) {
-                tmp->children[idx]->count = 1;
-            }
-            tmp = tmp->children[idx];
+            n = n->child[k];
         }
+        n->isEnd = true;
     }
 
     /** Returns if the word is in the trie. */
     bool search(string word) {
-        struct node *tmp = root;
-        for (int i = 0; i < word.size(); i++) {
-            int idx = word[i] - 'a';
-            if (tmp->children[idx] == NULL) {
+        auto n = root;
+        for (auto &x: word) {
+            auto k = x-'a';
+            if (n->child[k] == NULL) {
                 return false;
             }
-            tmp = tmp->children[idx];
+            n = n->child[k];
         }
-
-        return tmp != NULL && tmp->count > 0;
+        return n->isEnd == true;
     }
 
     /** Returns if there is any word in the trie that starts with the given prefix. */
     bool startsWith(string prefix) {
-        struct node *tmp = root;
-        for (int i = 0; i < prefix.size(); i++) {
-            int idx = prefix[i] - 'a';
-            if (tmp->children[idx] == NULL) {
+        auto n = root;
+        for (auto &x: prefix) {
+            auto k = x-'a';
+            if (n->child[k] == NULL) {
                 return false;
             }
-            tmp = tmp->children[idx];
+            n = n->child[k];
         }
-
         return true;
     }
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
