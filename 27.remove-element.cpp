31a7f4aa3e4c2e4b5c83ae58fe949cba2dad057d
sunznx <sunznx@163.com>
2021-04-19 00:46:08 +0800

    update 27.remove-element.cpp

---
+++ 27.remove-element.cpp
@@ -0,0 +1,13 @@
+// CreateTime: 2021-04-19 00:44:57
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int s = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[s++] = nums[i];
+            }
+        }
+        return s;
+    }
+};
