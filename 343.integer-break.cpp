96afb1dbd05dca6a7bcc7e39315841e12c630090
sunznx <sunznx@163.com>
2021-04-19 00:48:21 +0800

    update 343.integer-break.cpp

--- 343.integer-break.cpp
+++ 343.integer-break.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        if (n == 0 || n == 1 || n == 2) {
+        if (n == 2) {
             return 1;
         }
         if (n == 3) {
             return 2;
         }
-        if (n == 4) {
-            return 4;
-        }
 
-        int mod = n % 3;
-        if (mod == 0) {
-            return pow(3, n/3);
-        } else if (mod == 1) {
-            return pow(3, (n-3) / 3) * 4;
+        int a = n / 3;
+        int b = n % 3;
+        if (b == 0) {
+            return pow(3, a);
         }
-        return pow(3, (n-3) / 3) * 6;
+        if (b == 1) {
+            return pow(3, a-1) * 4;
+        }
+        return pow(3, a-1) * 6;
     }
 };
