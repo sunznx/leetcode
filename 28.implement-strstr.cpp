da5436fee58b719ab57a0f145980c8b6a47a9fe2
sunznx <sunznx@163.com>
2021-04-20 00:56:25 +0800

    update 28.implement-strstr.cpp

--- 28.implement-strstr.cpp
+++ 28.implement-strstr.cpp
@@ -1,25 +1,18 @@
+// CreateTime: 2021-04-20 00:46:30
 class Solution {
 public:
     int strStr(string haystack, string needle) {
         int len1 = haystack.size();
         int len2 = needle.size();
 
-        if (len2 > len1 || len2 == len1 && haystack != needle) {
-            return -1;
-        }
-
-        for (int j = 0; j <= len1-len2; j++) {
-            int ok = 1;
-            int index = j;
-            for (int i = 0; i < len2; i++) {
-                if (haystack[index++] != needle[i]) {
-                    ok = 0;
-                    break;
-                }
+        for (int i = 0; i <= len1-len2; i++) {
+            int k = 0;
+            while (k < len2 && haystack[i+k] == needle[k]) {
+                k++;
             }
 
-            if (ok) {
-                return j;
+            if (k == len2) {
+                return i;
             }
         }
 
