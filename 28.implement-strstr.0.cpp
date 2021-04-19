7f0c1fc8cff7bee6da60b19dccf039ce1e3bd62a
sunznx <sunznx@163.com>
2021-04-20 00:56:02 +0800

    update 28.implement-strstr.0.cpp

--- 28.implement-strstr.0.cpp
+++ 28.implement-strstr.0.cpp
@@ -4,15 +4,18 @@ public:
     int strStr(string haystack, string needle) {
         int len1 = haystack.size();
         int len2 = needle.size();
+
         for (int i = 0; i <= len1-len2; i++) {
             int k = 0;
             while (k < len2 && haystack[i+k] == needle[k]) {
                 k++;
             }
+
             if (k == len2) {
                 return i;
             }
         }
+
         return -1;
     }
 };
