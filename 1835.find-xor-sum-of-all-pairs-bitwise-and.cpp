f1dfc268ee8dd129dac01c0d687f26d66c59d245
sunznx <sunznx@163.com>
2021-04-19 15:46:29 +0800

    update 1835.find-xor-sum-of-all-pairs-bitwise-and.cpp

--- 1835.find-xor-sum-of-all-pairs-bitwise-and.cpp
+++ 1835.find-xor-sum-of-all-pairs-bitwise-and.cpp
@@ -1,16 +1,15 @@
 // CreateTime: 2021-04-18 10:25:13
 class Solution {
 public:
-    typedef long long LL;
     int getXORSum(vector<int>& arr1, vector<int>& arr2) {
-        LL len1 = arr1.size();
-        LL len2 = arr2.size();
+        int len1 = arr1.size();
+        int len2 = arr2.size();
 
-        LL x = 0;
-        LL y = 0;
+        int x = 0;
+        int y = 0;
 
-        vector<LL> m1(32);
-        vector<LL> m2(32);
+        vector<int> m1(32);
+        vector<int> m2(32);
         for (int i = 0; i < len1; i++) {
             for (int k = 0; k <= 31; k++) {
                 if ((arr1[i] & (1<<k))) {
@@ -28,10 +27,8 @@ public:
 
         int ans = 0;
         for (int k = 0; k <= 31; k++) {
-            if (m1[k] > 0 && m2[k] > 0) {
-                if (m1[k]*m2[k] % 2 == 1) {
-                    ans += (1<<k);
-                }
+            if (m1[k] % 2 == 1 && m2[k] % 2 == 1) {
+                ans += (1<<k);
             }
         }
         return ans;
