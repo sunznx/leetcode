f736c469749b1da899b6edd9cc471a8e32e7022d
sunznx <sunznx@163.com>
2021-04-21 01:32:53 +0800

    update 91.decode-ways.cpp

--- 91.decode-ways.cpp
+++ 91.decode-ways.cpp
@@ -10,31 +10,23 @@ public:
         return dfs(s);
     }
 
-    int dfs(string &s, int step = 0) {
+    int dfs(string &s, int step = 0, int state = 0) {
         if (step == s.size()) {
             return 1;
         }
 
-        int state = 0;
-        if (prev != 'x') {
-            state = 1;
-        }
-
         auto &k = m[state][step];
-
         if (k >= 0) {
             return k;
         }
 
         k = 0;
-        if ((prev == '1') || (prev == '2' && '0' <= s[step] && s[step] <= '6')) {
-            prev = 'x';
-            k += dfs(s, step+1);
+        if (state == 1 && ((s[step-1] == '1') || (s[step-1] == '2' && '0' <= s[step] && s[step] <= '6'))) {
+            k += dfs(s, step+1, 0);
         }
 
         if (s[step] != '0') {
-            prev = s[step];
-            k += dfs(s, step+1);
+            k += dfs(s, step+1, 1);
         }
 
         return k;
