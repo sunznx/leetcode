6e6ff348f1af618c58456fc14d243fb4c043d489
sunznx <sunznx@163.com>
2021-04-24 12:17:14 +0800

    update 377.combination-sum-iv.cpp

--- 377.combination-sum-iv.cpp
+++ 377.combination-sum-iv.cpp
@@ -1,21 +1,19 @@
 // CreateTime: 2021-04-24 00:14:39
 class Solution {
 public:
-
     typedef unsigned long long ULL;
 
     int combinationSum4(vector<int>& nums, int target) {
-        vector<ULL> dp(target+1, 0);
-
-        dp[0] = 1;
-        for (int i = 0; i <= target; i++) {
-            for (int j = 0; j < nums.size(); j++) {
-                auto x = nums[j];
-                if (i >= x) {
-                    dp[i] += dp[i-x];
+        vector<ULL> f(target+1, 0);
+        f[0] = 1;
+        for (int j = 0; j <= target; j++) {
+            for (auto &x: nums) {
+                if (j >= x) {
+                    f[j] += f[j-x];
                 }
             }
         }
-        return dp[target];
+        return f[target];
     }
 };
+
