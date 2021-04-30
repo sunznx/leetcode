4b150183bc5adfd7e228d1c4d9c5c65634b55f42
sunznx <sunznx@163.com>
2021-04-30 10:57:06 +0800

    update 137.single-number-ii.cpp

--- 137.single-number-ii.cpp
+++ 137.single-number-ii.cpp
@@ -2,22 +2,20 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<int> m(32);
-
-        for (auto &x: nums) {
-            for (int i = 0; i < 32; i++) {
-                if ((x & (1<<i)) != 0) {
-                    m[i]++;
-                }
-            }
-        }
-
         int ans = 0;
         for (int i = 0; i < 32; i++) {
-            if (m[i] % 3 != 0) {
+            int m = 0;
+            for (auto &x: nums) {
+                if ((x & (1<<i)) != 0) {
+                    m++;
+                }                
+            }
+            
+            if (m % 3 != 0) {
                 ans += (1<<i);
             }
         }
+
         return ans;
     }
 };
